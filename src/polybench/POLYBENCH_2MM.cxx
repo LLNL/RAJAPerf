5aef0d5fe593837bb1a626141a9570881634ce95
Holger E. Jones <jones19@llnl.gov>
2017-05-10 14:37:31 -0700

    fleshed out 2MM kernel body for Baseline Sequential

--- src/polybench/POLYBENCH_2MM.cxx
+++ src/polybench/POLYBENCH_2MM.cxx
@@ -35,39 +35,54 @@ namespace rajaperf
 namespace polybench
 {
 
-#define POLYBENCH_2MM_DATA 
+#define POLYBENCH_2MM_DATA \
+  ResReal_ptr tmp = m_tmp; \
+  ResReal_ptr A = m_A; \
+  ResReal_ptr B = m_B; \
+  ResReal_ptr C = m_C; \
+  ResReal_ptr D = m_D; \
+  const Real_type alpha = m_alpha; \
+  const Real_type beta = m_beta; 
 
-#define POLYBENCH_2MM_BODY
+// The following 2MM_BODY is a prototype of the kernel copied over from the polybench suite and is not used in the runKernel calls  
+// It's just for illustrative purposes
+#define POLYBENCH_2MM_BODY \
+  int i, j, k;\
+  /* D := alpha*A*B*C + beta*D */ \
+  for (i = 0; i < m_ni; i++) \
+    for (j = 0; j < m_nj; j++) { \
+	      m_tmp[i][j] = 0.0; \
+	      for (k = 0; k < m_nk; ++k) \
+	        m_tmp[i][j] += m_alpha * m_A[i][k] * m_B[k][j]; \
+    } \
+  for (i = 0; i < m_ni; i++) \
+    for (j = 0; j < m_nl; j++) { \
+	    m_D[i][j] *= m_beta; \
+	    for (k = 0; k < m_nj; ++k) \
+	      m_D[i][j] += m_tmp[i][k] * m_C[k][j]; \
+    } 
+
+#define POLYBENCH_2MM_BODY1 \
+  *(tmp + i * nj + j) = 0.0;
+
+#define POLYBENCH_2MM_BODY2 \
+  *(tmp + i * nj + j) += alpha * *(A + i * nk + k) * *(B + k * nj + j);
+
+#define POLYBENCH_2MM_BODY3 \
+  *(D + i * nj + j) *= beta;
+
+#define POLYBENCH_2MM_BODY4 \
+  *(D + i * nj + j) += *(tmp + i * nk + k) * *(C + k * nj + j);
+
+
+
+  
 
 
 POLYBENCH_2MM::POLYBENCH_2MM(const RunParams& params)
   : KernelBase(rajaperf::Polybench_2MM, params)
 {
-#if 0
-  SizeSpec_T lsizespec = run_params.getSizeSpec();
-   
-  setDefaultSamples(100);
-  switch(lsizespec) {
-    case Mini:
-      m_ni=16; m_nj=18; m_nk=22; m_nl=24;
-      break;
-    case Small:
-      m_ni=40; m_nj=50; m_nk=70; m_nl=80;
-      break;
-    case Medium:
-      m_ni=180; m_nj=190; m_nk=210; m_nl=220;
-      break;
-    case Large:
-      m_ni=800; m_nj=900; m_nk=1100; m_nl=1200;
-      break;
-    case Extralarge:
-      m_ni=1600; m_nj=1800; m_nk=2200; m_nl=2400;
-      break;
-    default:
-      m_ni=180; m_nj=190; m_nk=210; m_nl=220;
-      break;
-  }
-#endif  
+  setDefaultSamples(10);
 }
 
 POLYBENCH_2MM::~POLYBENCH_2MM() 
@@ -76,9 +91,11 @@ POLYBENCH_2MM::~POLYBENCH_2MM()
 
 void POLYBENCH_2MM::setUp(VariantID vid)
 {
+
+  m_alpha = 1.5;
+  m_beta = 1.2;
   SizeSpec_T lsizespec = KernelBase::getSizeSpec();
    
-  setDefaultSamples(100);
   switch(lsizespec) {
     case Mini:
       m_ni=16; m_nj=18; m_nk=22; m_nl=24;
@@ -108,10 +125,11 @@ void POLYBENCH_2MM::setUp(VariantID vid)
 
 void POLYBENCH_2MM::runKernel(VariantID vid)
 {
-#if 0
-  Index_type run_size = getRunSize();
-#endif
   const Index_type run_samples = getRunSamples();
+  const Index_type ni = m_ni;
+  const Index_type nj = m_nj;
+  const Index_type nk = m_nk;
+  const Index_type nl = m_nl;
 
   switch ( vid ) {
 
@@ -121,10 +139,20 @@ void POLYBENCH_2MM::runKernel(VariantID vid)
 
       startTimer();
       for (SampIndex_type isamp = 0; isamp < run_samples; ++isamp) {
-#if 0
-        for (Index_type i = 0; i < run_size; ++i ) {
-          POLYBENCH_2MM_BODY;
-        }
+#if 1
+        for (Index_type i = 0; i < ni; ++i ) 
+          for(Index_type j = 0; j < nj; ++j) {
+            POLYBENCH_2MM_BODY1;
+            for(Index_type k = 0; k < nk; ++k)
+              POLYBENCH_2MM_BODY2;
+          }
+        for(Index_type i = 0; i < ni; ++i)
+          for(Index_type j = 0; j < nl; ++j) {
+            POLYBENCH_2MM_BODY3;
+            for(Index_type k = 0; k < nj; ++k)
+              POLYBENCH_2MM_BODY4;
+          }  
+        
 #endif
 
       }
