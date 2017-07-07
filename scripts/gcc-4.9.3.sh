6866c02d2f43bd84ce489b31e64cdcf7b1e1e7dc
Rich Hornung <hornung1@llnl.gov>
2017-07-07 16:38:45 -0700

    Enabled warning compilation mode and added some scripts to show CMake usage.

---
+++ scripts/gcc-4.9.3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##
+## Copyright (c) 2016, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-689114
+##
+## All rights reserved.
+##
+## For additional details and restrictions, please see RAJA/LICENSE.txt
+##
+
+rm -rf build-gcc-4.9.3 2>/dev/null
+mkdir build-gcc-4.9.3 && cd build-gcc-4.9.3
+
+PERFSUITE_DIR=$(git rev-parse --show-toplevel)
+
+RAJA_DIR=${PERFSUITE_DIR}/tpl/RAJA
+
+cmake \
+  -C ${RAJA_DIR}/host-configs/chaos/gcc_4_9_3.cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DENABLE_OPENMP=On \
+  -DENABLE_ALL_WARNINGS=Off \
+  -DCMAKE_INSTALL_PREFIX=../install-gcc-4.9.3 \
+  "$@" \
+  ${PERFSUITE_DIR}
