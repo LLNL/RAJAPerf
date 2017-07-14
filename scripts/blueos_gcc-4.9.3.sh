c257a4bcf3d53c0ecf80073db4f79eff89df5b1f
Rich Hornung <hornung1@llnl.gov>
2017-07-14 14:00:33 -0700

    Basic files for blueos machines.
    nvcc still not working....

---
+++ scripts/blueos_gcc-4.9.3.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##
+## Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-xxxxxx
+##
+## All rights reserved.
+##
+## This file is part of the RAJA Performance Suite.
+##
+## For more information, see the file LICENSE in the top-level directory.
+##
+
+rm -rf build_blueos-gcc-4.9.3 2>/dev/null
+mkdir build_blueos-gcc-4.9.3 && cd build_blueos-gcc-4.9.3
+
+module load gcc/4.9.3
+
+PERFSUITE_DIR=$(git rev-parse --show-toplevel)
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DENABLE_OPENMP=On \
+  -DPERFSUITE_ENABLE_WARNINGS=Off \
+  -DENABLE_ALL_WARNINGS=Off \
+  -DCMAKE_INSTALL_PREFIX=../install_blueos-gcc-4.9.3 \
+  "$@" \
+  ${PERFSUITE_DIR}
