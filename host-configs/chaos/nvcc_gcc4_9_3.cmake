beb9f7ff34d406872782e8354bb7ae32a2beb333
Rich Hornung <hornung1@llnl.gov>
2017-07-14 13:06:53 -0700

    First crack at getting nvcc to work (still have issues)
    Switched to new RAJA and BLT branches for submodules.

--- host-configs/chaos/nvcc_gcc4_9_3.cmake
+++ host-configs/chaos/nvcc_gcc4_9_3.cmake
@@ -17,19 +17,18 @@ set(CMAKE_CXX_COMPILER "/usr/apps/gnu/4.9.3/bin/g++" CACHE PATH "")
 
 set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -Ofast -mavx -finline-functions -finline-limit=20000" CACHE STRING "")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -Ofast -mavx -finline-functions -finline-limit=20000" CACHE STRING "")
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -Wall -Werror -Wextra" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0" CACHE STRING "")
 
 if(CMAKE_BUILD_TYPE MATCHES Release)
-  set(RAJA_NVCC_FLAGS -O2; -restrict; -arch compute_35; -std c++11; --expt-extended-lambda; -ccbin; ${CMAKE_CXX_COMPILER} CACHE LIST "")
+#  set(BLT_CUDA_FLAGS "-O2 -restrict -arch compute_37 -std c++11 --expt-extended-lambda -ccbin" ${CMAKE_CXX_COMPILER} CACHE LIST "")
+#   set(BLT_CUDA_FLAGS -O2; -restrict; -arch compute_37; -std c++11; --expt-extended-lambda; -ccbin; ${CMAKE_CXX_COMPILER} CACHE LIST "")
+#  set(RAJA_NVCC_FLAGS -O2; -restrict; -arch compute_37; -std c++11; --expt-extended-lambda; -ccbin; ${CMAKE_CXX_COMPILER} CACHE LIST "")
 elseif(CMAKE_BUILD_TYPE MATCHES RelWithDebInfo)
-  set(RAJA_NVCC_FLAGS -g; -G; -O2; -restrict; -arch compute_35; -std c++11; --expt-extended-lambda; -ccbin ${CMAKE_CXX_COMPILER} CACHE LIST "")
+  set(RAJA_NVCC_FLAGS -g; -G; -O2; -restrict; -arch compute_37; -std c++11; --expt-extended-lambda; -ccbin ${CMAKE_CXX_COMPILER} CACHE LIST "")
 elseif(CMAKE_BUILD_TYPE MATCHES Debug)
-  set(RAJA_NVCC_FLAGS -g; -G; -O0; -restrict; -arch compute_35; -std c++11; --expt-extended-lambda; -ccbin ${CMAKE_CXX_COMPILER} CACHE LIST "")
+  set(RAJA_NVCC_FLAGS -g; -G; -O0; -restrict; -arch compute_37; -std c++11; --expt-extended-lambda; -ccbin ${CMAKE_CXX_COMPILER} CACHE LIST "")
 endif()
 
-
-set(RAJA_ENABLE_CUDA On CACHE BOOL "")
-set(RAJA_ENABLE_OPENMP On CACHE BOOL "")
 
 set(RAJA_RANGE_ALIGN 4 CACHE INT "")
 set(RAJA_RANGE_MIN_LENGTH 32 CACHE INT "")
---
+++ scripts/chaos_nvcc8.0_gcc4.9.3.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##
+## Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-xxxxxx
+##
+## All rights reserved.
+##
+## This file is part of the RAJA Performance Suite.
+##
+## For more information, see the file LICENSE in the top-level directory.
+##
+
+rm -rf build_chaos-nvcc8.0_gcc4.9.3 2>/dev/null
+mkdir build_chaos-nvcc8.0_gcc4.9.3 && cd build_chaos-nvcc8.0_gcc4.9.3
+
+PERFSUITE_DIR=$(git rev-parse --show-toplevel)
+
+cmake \
+  -C ${PERFSUITE_DIR}/host-configs/chaos/nvcc_gcc4_9_3.cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DENABLE_OPENMP=On \
+  -DENABLE_CUDA=On \
+  -DCUDA_TOOLKIT_ROOT_DIR=/opt/cudatoolkit-8.0 \
+  -DPERFSUITE_ENABLE_WARNINGS=Off \
+  -DENABLE_ALL_WARNINGS=Off \
+  -DRAJA_ENABLE_TESTS=Off \
+  -DCMAKE_INSTALL_PREFIX=../install_chaos-gcc-4.9.3 \
+  "$@" \
+  ${PERFSUITE_DIR}
