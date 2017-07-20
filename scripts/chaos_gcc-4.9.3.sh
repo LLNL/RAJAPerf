3d6685550d5936f329277feabffaa57cb24e5639
Rich Hornung <hornung1@llnl.gov>
2017-07-20 16:22:06 -0700

    Fixed issue with 'use cmake' command.

--- scripts/chaos_gcc-4.9.3.sh
+++ scripts/chaos_gcc-4.9.3.sh
@@ -16,7 +16,7 @@
 
 rm -rf build_chaos-gcc-4.9.3 2>/dev/null
 mkdir build_chaos-gcc-4.9.3 && cd build_chaos-gcc-4.9.3
-. /usr/local/tools/dotkit/init.sh && use cmake-3.4.1p
+. /usr/local/tools/dotkit/init.sh && use cmake-3.4.1
 
 PERFSUITE_DIR=$(git rev-parse --show-toplevel)
 
