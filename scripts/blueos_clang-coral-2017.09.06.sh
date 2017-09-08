4d332211a63db5a7463e837192361437c887301a
Rich Hornung <hornung1@llnl.gov>
2017-09-08 13:46:40 -0700

    Added scripts to build with latest clang compiler drop on blueos.

---
+++ scripts/blueos_clang-coral-2017.09.06.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##
+## Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-xxxxxx
+##
+## All rights reserved.
+##
+## This file is part of the RAJA Performance Suite.
+##
+## For more information, see the file LICENSE in the top-level directory.
+##
+
+rm -rf build_blueos-clang-coral-2017.09.06 2>/dev/null
+mkdir build_blueos-clang-coral-2017.09.06 && cd build_blueos-clang-coral-2017.09.06
+
+module load cmake/3.7.2
+
+PERFSUITE_DIR=$(git rev-parse --show-toplevel)
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -C ${PERFSUITE_DIR}/host-configs/blueos/clang_coral_2017_09_06.cmake \
+  -DENABLE_OPENMP=On \
+  -DENABLE_TBB=Off \
+  -DPERFSUITE_ENABLE_WARNINGS=Off \
+  -DENABLE_ALL_WARNINGS=Off \
+  -DCMAKE_INSTALL_PREFIX=../install_blueos-clang-coral-2017.09.06 \
+  "$@" \
+  ${PERFSUITE_DIR}
