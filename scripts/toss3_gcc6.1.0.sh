a53f35dd40a9941c26b6b59d6158833d1137a0cf
Rich Hornung <hornung1@llnl.gov>
2017-10-06 14:12:34 -0700

    Toss 3 builds: updated Intel18 to released version, added gnu 6 and 7

---
+++ scripts/toss3_gcc6.1.0.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##
+## Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-738930
+##
+## All rights reserved.
+## 
+## This file is part of the RAJA Performance Suite.
+##
+## For details about use and distribution, please read raja-perfsuite/LICENSE.
+##
+
+rm -rf build_toss3-gcc-6.1.0 2>/dev/null
+mkdir build_toss3-gcc-6.1.0 && cd build_toss3-gcc-6.1.0
+
+module load cmake/3.5.2
+
+PERFSUITE_DIR=$(git rev-parse --show-toplevel)
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -C ${PERFSUITE_DIR}/host-configs/toss3/gcc_6_1_0.cmake \
+  -DENABLE_OPENMP=On \
+  -DPERFSUITE_ENABLE_WARNINGS=Off \
+  -DENABLE_ALL_WARNINGS=Off \
+  -DCMAKE_INSTALL_PREFIX=../install_toss3-gcc-6.1.0 \
+  "$@" \
+  ${PERFSUITE_DIR}
--- scripts/toss3_icpc18.0.0.sh
+++ scripts/toss3_icpc18.0.0.sh
@@ -14,20 +14,20 @@
 ## For details about use and distribution, please read raja-perfsuite/LICENSE.
 ##
 
-rm -rf build_toss3-icpc-18.0-beta 2>/dev/null
-mkdir build_toss3-icpc-18.0-beta && cd build_toss3-icpc-18.0-beta
+rm -rf build_toss3-icpc-18.0.0 2>/dev/null
+mkdir build_toss3-icpc-18.0.0 && cd build_toss3-icpc-18.0.0
 
 module load cmake/3.5.2
-module load gcc/4.9.3
+module load gcc/7.1.0
 
 PERFSUITE_DIR=$(git rev-parse --show-toplevel)
 
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -C ${PERFSUITE_DIR}/host-configs/toss3/icpc_18_0_beta.cmake \
+  -C ${PERFSUITE_DIR}/host-configs/toss3/icpc_18_0_0.cmake \
   -DENABLE_OPENMP=On \
   -DPERFSUITE_ENABLE_WARNINGS=Off \
   -DENABLE_ALL_WARNINGS=Off \
-  -DCMAKE_INSTALL_PREFIX=../install_toss3-icpc-18.0-beta \
+  -DCMAKE_INSTALL_PREFIX=../install_toss3-icpc-18.0.0 \
   "$@" \
   ${PERFSUITE_DIR}
