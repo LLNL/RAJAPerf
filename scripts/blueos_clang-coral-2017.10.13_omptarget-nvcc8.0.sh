aedeb889df409dfddc120a3cd53b7f05b1444b93
Rich Hornung <hornung1@llnl.gov>
2017-11-29 15:09:52 -0800

    Cleaned up bluos scripts.
    Removed some -- only keep one clang-coral drop per month.
    Removed unneeded CMake options and module loads.
    Renamed clang-coral scripts for omptarget builds.

--- scripts/blueos_clang-coral-2017.10.13_omptarget-nvcc8.0.sh
+++ scripts/blueos_clang-coral-2017.10.13_omptarget-nvcc8.0.sh
@@ -14,12 +14,10 @@
 ## For details about use and distribution, please read raja-perfsuite/LICENSE.
 ##
 
-rm -rf build_blueos_nvcc8.0_clang-coral-2017.10.13 >/dev/null
-mkdir build_blueos_nvcc8.0_clang-coral-2017.10.13 && cd build_blueos_nvcc8.0_clang-coral-2017.10.13
+rm -rf blueos_clang-coral-2017.10.13_omptarget-nvcc8.0 >/dev/null
+mkdir blueos_clang-coral-2017.10.13_omptarget-nvcc8.0 && cd blueos_clang-coral-2017.10.13_omptarget-nvcc8.0
 
 module load cmake/3.9.2
-module load clang/coral-2017.10.13
-module load cuda/8.0
 
 PERFSUITE_DIR=$(git rev-parse --show-toplevel)
 
@@ -28,11 +26,11 @@ cmake \
   -C ${PERFSUITE_DIR}/host-configs/blueos/clang_coral_2017_10_13.cmake \
   -DENABLE_OPENMP=On \
   -DENABLE_CUDA=Off \
-  -DOpenMP_CXX_FLAGS="-fopenmp -fopenmp-targets=nvptx64-nvidia-cuda -fopenmp-implicit-declare-target" \
-  -DRAJA_ENABLE_TARGET_OPENMP=On \
   -DENABLE_TARGET_OPENMP=On \
+  -DOpenMP_CXX_FLAGS="-fopenmp -fopenmp-targets=nvptx64-nvidia-cuda -fopenmp-implicit-declare-target" \
+  -DCUDA_TOOLKIT_ROOT_DIR=/usr/tcetmp/packages/cuda-8.0 \
   -DPERFSUITE_ENABLE_WARNINGS=Off \
   -DENABLE_ALL_WARNINGS=Off \
-  -DCMAKE_INSTALL_PREFIX=../install_blueos_nvcc8.0_clang-coral-2017.10.13 \
+  -DCMAKE_INSTALL_PREFIX=../install_blueos_clang-coral-2017.10.13_omptarget-nvcc8.0 \
   "$@" \
   ${PERFSUITE_DIR}
