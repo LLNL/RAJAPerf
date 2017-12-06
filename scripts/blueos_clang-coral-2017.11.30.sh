8ca00f24bcf401a2d629e1a0c5d545286a5fc646
Matthew Jean Cordery <mcorder@us.ibm.com>
2017-12-06 12:08:27 -0800

    "updated vol3d.cpp"

---
+++ scripts/blueos_clang-coral-2017.11.30.sh
@@ -0,0 +1,39 @@
+
+##
+## Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-738930
+##
+## All rights reserved.
+##
+## This file is part of the RAJA Performance Suite.
+##
+## For details about use and distribution, please read raja-perfsuite/LICENSE.
+##
+
+
+rm -rf build_blueos_clang-coral-2017.11.30 >/dev/null
+mkdir build_blueos_clang-coral-2017.11.30 && cd build_blueos_clang-coral-2017.11.30
+
+
+export CUDA_DIR=$CUDA_HOME
+
+PERFSUITE_DIR=$(git rev-parse --show-toplevel)
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -C ${PERFSUITE_DIR}/host-configs/blueos/clang_coral_2017_11_30.cmake \
+  -DENABLE_OPENMP=On \
+  -DENABLE_CUDA=Off \
+  -DOpenMP_CXX_FLAGS="-v -fopenmp -fopenmp-targets=nvptx64-nvidia-cuda -fopenmp-implicit-declare-target -fopenmp-implicit-map-lambdas" \
+  -DENABLE_TARGET_OPENMP=On \
+  -DRAJA_ENABLE_TARGET_OPENMP=On \
+  -DRAJA_ENABLE_MODULES=Off \
+  -DRAJA_ENABLE_TESTS=Off \
+  -DPERFSUITE_ENABLE_WARNINGS=Off \
+  -DENABLE_ALL_WARNINGS=Off \
+  -DCMAKE_INSTALL_PREFIX=../install_blueos_clang-coral-2017.11.30 \
+  "$@" \
+  ${PERFSUITE_DIR}
---
+++ scripts/blueos_nvcc9.0_xl-beta-2017.11.28.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+##
+## Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-738930
+##
+## All rights reserved.
+## 
+## This file is part of the RAJA Performance Suite.
+##
+## For details about use and distribution, please read raja-perfsuite/LICENSE.
+##
+
+rm -rf build_blueos_nvcc9.0_xl-beta-2017.11.28 >/dev/null
+mkdir build_blueos_nvcc9.0_xl-beta-2017.11.28 && cd build_blueos_nvcc9.0_xl-beta-2017.11.28
+
+module load cmake/3.7.2
+
+PERFSUITE_DIR=$(git rev-parse --show-toplevel)
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -C ${PERFSUITE_DIR}/host-configs/blueos/nvcc_xl-beta-2017.11.28.cmake \
+  -DENABLE_MODULES=Off \
+  -DENABLE_TESTS=Off \
+  -DENABLE_OPENMP=On \
+  -DENABLE_TARGET_OPENMP=On \
+  -DRAJA_ENABLE_TARGET_OPENMP=On \
+  -DOpenMP_CXX_FLAGS="-qsmp=omp -qoffload" \
+  -DENABLE_CUDA=Off \
+  -DCUDA_TOOLKIT_ROOT_DIR=/usr/tcetmp/packages/cuda-9.0.176 \
+  -DPERFSUITE_ENABLE_WARNINGS=Off \
+  -DENABLE_ALL_WARNINGS=Off \
+  -DCMAKE_INSTALL_PREFIX=../install_blueos_nvcc9.0_xl-beta-2017.11.28 \
+  "$@" \
+  ${PERFSUITE_DIR}
