0ff12e2737b5434f2bdf9b46f616342acb6480d0
Rich Hornung <hornung1@llnl.gov>
2018-02-01 12:36:50 -0800

    Updated copyright year in all files.

--- scripts/blueos_nvcc8.0_clang-coral-2017.09.18.sh
+++ scripts/blueos_nvcc8.0_clang-coral-2017.09.18.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##
-## Copyright (c) 2017, Lawrence Livermore National Security, LLC.
+## Copyright (c) 2017-18, Lawrence Livermore National Security, LLC.
 ##
 ## Produced at the Lawrence Livermore National Laboratory.
 ##
