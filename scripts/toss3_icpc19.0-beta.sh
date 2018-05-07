31ab5f684773a835ec3844dd3dcd951ab9e84b19
Rich Hornung <hornung1@llnl.gov>
2018-05-07 15:26:45 -0700

    Updated and added toss3 intel build files.

--- scripts/toss3_icpc19.0-beta.sh
+++ scripts/toss3_icpc19.0-beta.sh
@@ -14,18 +14,18 @@
 ## For details about use and distribution, please read raja-perfsuite/LICENSE.
 ##
 
-rm -rf build_toss3-icpc-18.0.2 2>/dev/null
-mkdir build_toss3-icpc-18.0.2 && cd build_toss3-icpc-18.0.2
+rm -rf build_toss3-icpc-19.0-beta 2>/dev/null
+mkdir build_toss3-icpc-19.0-beta && cd build_toss3-icpc-19.0-beta
 
 module load cmake/3.5.2
 module load gcc/7.1.0
 
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -C ../host-configs/toss3/icpc_18_0_2.cmake \
+  -C ../host-configs/toss3/icpc_19_0_beta.cmake \
   -DENABLE_OPENMP=On \
   -DPERFSUITE_ENABLE_WARNINGS=Off \
   -DENABLE_ALL_WARNINGS=Off \
-  -DCMAKE_INSTALL_PREFIX=../install_toss3-icpc-18.0.2 \
+  -DCMAKE_INSTALL_PREFIX=../install_toss3-icpc-19.0-beta \
   "$@" \
   ..
