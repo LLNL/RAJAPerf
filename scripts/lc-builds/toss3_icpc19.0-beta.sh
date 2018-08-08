2164f48c524adca1b46cedece23aa47e55799c3e
Rich Hornung <hornung1@llnl.gov>
2018-08-08 16:20:31 -0700

    New set of build scripts that use RAJA host config files.

---
+++ scripts/lc-builds/toss3_icpc19.0-beta.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##
+## Copyright (c) 2017-18, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-738930
+##
+## All rights reserved.
+##
+## This file is part of the RAJA Performance Suite.
+##
+## For details about use and distribution, please read raja-perfsuite/LICENSE.
+##
+
+BUILD_SUFFIX=lc_toss3-icpc-19.0-beta
+RAJA_HOSTCONFIG=../tpl/RAJA/host-configs/lc-builds/toss3/icpc_19_0_beta.cmake
+
+rm -rf build_${BUILD_SUFFIX} 2>/dev/null
+mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
+
+module load cmake/3.9.2
+module load gcc/7.1.0
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -C ${RAJA_HOSTCONFIG} \
+  -DENABLE_OPENMP=On \
+  -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
+  "$@" \
+  ..
---
+++ scripts/lc-builds/toss3_nvcc9.1.85_gcc4.9.3.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##
+## Copyright (c) 2017-18, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-738930
+##
+## All rights reserved.
+##
+## This file is part of the RAJA Performance Suite.
+##
+## For details about use and distribution, please read raja-perfsuite/LICENSE.
+##
+
+BUILD_SUFFIX=lc_toss3-nvcc9.1.85_gcc4.9.3
+RAJA_HOSTCONFIG=../tpl/RAJA/host-configs/lc-builds/toss3/nvcc_gcc4_9_3.cmake
+
+rm -rf build_${BUILD_SUFFIX} 2>/dev/null
+mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
+
+module load cmake/3.9.2
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release \
+  -C ${RAJA_HOSTCONFIG} \
+  -DENABLE_OPENMP=On \
+  -DENABLE_CUDA=On \
+  -DCUDA_TOOLKIT_ROOT_DIR=/usr/tce/packages/cuda/cuda-9.1.85 \
+  -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
+  "$@" \
+  ..
