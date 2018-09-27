dc8472158cebdbb6f490f1f2d9af051efd346d2d
Rich Hornung <hornung1@llnl.gov>
2018-09-27 13:23:08 -0700

    Trying to get travis to build with nvcc.

--- scripts/travis_build_and_test.sh
+++ scripts/travis_build_and_test.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 env
 function or_die () {
@@ -16,13 +17,15 @@ or_die mkdir travis-build
 cd travis-build
 if [[ "$DO_BUILD" == "yes" ]] ; then
     or_die cmake -DCMAKE_CXX_COMPILER="${COMPILER}" ${CMAKE_EXTRA_FLAGS} ../
+# for debugging...
+#   cat CMakeCache.txt
     if [[ ${CMAKE_EXTRA_FLAGS} == *COVERAGE* ]] ; then
       or_die make -j 3
     else
       or_die make -j 3 VERBOSE=1
     fi
     if [[ "${DO_TEST}" == "yes" ]] ; then
-      or_die ctest -V
+      or_die ./bin/raja-perf.exe --checkrun -sp
     fi
 fi
 
