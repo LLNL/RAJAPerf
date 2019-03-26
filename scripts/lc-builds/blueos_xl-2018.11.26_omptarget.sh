41a09795d8f4546445f745ba1bf03ac725fef237
Rich Hornung <hornung1@llnl.gov>
2019-03-26 15:42:54 -0700

    New builds scripts based on RAJA host-configs.

---
+++ scripts/lc-builds/blueos_xl-2018.11.26_omptarget.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+##
+## Copyright (c) 2017-19, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-738930
+##
+## All rights reserved.
+##
+## This file is part of the RAJA Performance Suite.
+##
+## For details about use and distribution, please read RAJAPerf/LICENSE.
+##
+
+BUILD_SUFFIX=lc_blueos-xl_2018.11.26_omptarget
+RAJA_HOSTCONFIG=../tpl/RAJA/host-configs/lc-builds/blueos/xl_2018_11_26.cmake
+
+rm -rf build_${BUILD_SUFFIX} 2>/dev/null
+mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
+
+module load cmake/3.9.2
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release\
+  -C ${RAJA_HOSTCONFIG} \
+  -DENABLE_OPENMP=On \
+  -DENABLE_TARGET_OPENMP=On \
+  -DOpenMP_CXX_FLAGS="-std=c++11;-qoffload;-qsmp=omp;-qnoeh;-qnoinline;-qalias=noansi" \
+  -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
+  "$@" \
+  ..
---
+++ scripts/lc-builds/blueos_xl-2019.02.07.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+##
+## Copyright (c) 2017-19, Lawrence Livermore National Security, LLC.
+##
+## Produced at the Lawrence Livermore National Laboratory.
+##
+## LLNL-CODE-738930
+##
+## All rights reserved.
+##
+## This file is part of the RAJA Performance Suite.
+##
+## For details about use and distribution, please read RAJAPerf/LICENSE.
+##
+
+BUILD_SUFFIX=lc_blueos-xl_2019.02.07
+RAJA_HOSTCONFIG=../tpl/RAJA/host-configs/lc-builds/blueos/xl_2019_02_07.cmake
+
+rm -rf build_${BUILD_SUFFIX} 2>/dev/null
+mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
+
+module load cmake/3.9.2
+
+cmake \
+  -DCMAKE_BUILD_TYPE=Release\
+  -C ${RAJA_HOSTCONFIG} \
+  -DENABLE_OPENMP=On \
+  -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
+  "$@" \
+  ..
