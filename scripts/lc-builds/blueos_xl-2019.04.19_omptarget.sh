e1a102e9071dfd25a1e330eee39276c0f63088b8
Rich Hornung <hornung1@llnl.gov>
2019-10-14 16:26:07 -0700

    updating blueos xl build scripts.

--- scripts/lc-builds/blueos_xl-2019.04.19_omptarget.sh
+++ scripts/lc-builds/blueos_xl-2019.04.19_omptarget.sh
@@ -8,16 +8,17 @@
 # SPDX-License-Identifier: (BSD-3-Clause)
 #################################################################################
 
-BUILD_SUFFIX=lc_blueos-xl-test_2019.03.12_omptarget
-RAJA_HOSTCONFIG=../tpl/RAJA/host-configs/lc-builds/blueos/xl_test_2019_03_12.cmake
+BUILD_SUFFIX=lc_blueos-xl_2019.04.19_omptarget
+RAJA_HOSTCONFIG=../tpl/RAJA/host-configs/lc-builds/blueos/xl_2019_X.cmake
 
 rm -rf build_${BUILD_SUFFIX} 2>/dev/null
 mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
 
-module load cmake/3.9.2
+module load cmake/3.14.5
 
 cmake \
-  -DCMAKE_BUILD_TYPE=Release\
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_COMPILER=/usr/tce/packages/xl/xl-2019.04.19/bin/xlc++_r \
   -C ${RAJA_HOSTCONFIG} \
   -DENABLE_OPENMP=On \
   -DENABLE_TARGET_OPENMP=On \
--- scripts/lc-builds/blueos_xl-2019.06.12_omptarget.sh
+++ scripts/lc-builds/blueos_xl-2019.06.12_omptarget.sh
@@ -8,18 +8,21 @@
 # SPDX-License-Identifier: (BSD-3-Clause)
 #################################################################################
 
-BUILD_SUFFIX=lc_blueos-xl-beta_2018.10.29
-RAJA_HOSTCONFIG=../tpl/RAJA/host-configs/lc-builds/blueos/xl_beta_2018_10_29.cmake
+BUILD_SUFFIX=lc_blueos-xl_2019.06.12_omptarget
+RAJA_HOSTCONFIG=../tpl/RAJA/host-configs/lc-builds/blueos/xl_2019_X.cmake
 
 rm -rf build_${BUILD_SUFFIX} 2>/dev/null
 mkdir build_${BUILD_SUFFIX} && cd build_${BUILD_SUFFIX}
 
-module load cmake/3.9.2
+module load cmake/3.14.5
 
 cmake \
-  -DCMAKE_BUILD_TYPE=Release\
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_CXX_COMPILER=/usr/tce/packages/xl/xl-2019.06.12/bin/xlc++_r \
   -C ${RAJA_HOSTCONFIG} \
   -DENABLE_OPENMP=On \
+  -DENABLE_TARGET_OPENMP=On \
+  -DOpenMP_CXX_FLAGS="-qoffload;-qsmp=omp;-qnoeh;-qalias=noansi" \
   -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
   "$@" \
   ..
