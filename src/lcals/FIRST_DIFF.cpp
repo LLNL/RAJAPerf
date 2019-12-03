32f473db4536e4f22e34e626fbd35458ced7ff74
Rich Hornung <hornung1@llnl.gov>
2019-12-03 15:48:56 -0800

    Add Lambda_Seq variant.

--- src/lcals/FIRST_DIFF.cpp
+++ src/lcals/FIRST_DIFF.cpp
@@ -72,7 +72,22 @@ void FIRST_DIFF::runKernel(VariantID vid)
       break;
     }
 
-#if defined(RUN_RAJA_SEQ)     
+#if defined(RUN_RAJA_SEQ)
+    case Lambda_Seq : {
+
+      startTimer();
+      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
+
+        for (Index_type i = ibegin; i < iend; ++i ) {
+          firstdiff_lam(i);
+        }
+
+      }
+      stopTimer();
+
+      break;
+    }
+
     case RAJA_Seq : {
 
       startTimer();
--- src/lcals/INT_PREDICT.cpp
+++ src/lcals/INT_PREDICT.cpp
@@ -91,7 +91,22 @@ void INT_PREDICT::runKernel(VariantID vid)
       break;
     }
 
-#if defined(RUN_RAJA_SEQ)     
+#if defined(RUN_RAJA_SEQ)
+    case Lambda_Seq : {
+
+      startTimer();
+      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
+
+        for (Index_type i = ibegin; i < iend; ++i ) {
+          INT_PREDICT_BODY;
+        }
+
+      }
+      stopTimer();
+
+      break;
+    }
+
     case RAJA_Seq : {
 
       startTimer();
