f8fe45b2280e5db543a2538de19c4cf96201a65d
Rich Hornung <hornung1@llnl.gov>
2019-12-10 14:46:41 -0800

    Changes to allow refactoring to continue.
    If we follow the same refactoring pattern for the FDTD_2D kernel that
    we did for all the others, the XL compiler generates an internal
    compiler error. Issue has been submitted to IBM.

--- src/polybench/POLYBENCH_FDTD_2D.hpp
+++ src/polybench/POLYBENCH_FDTD_2D.hpp
@@ -109,6 +109,9 @@ public:
   void runKernel(VariantID vid); 
   void updateChecksum(VariantID vid);
   void tearDown(VariantID vid);
+
+  void runSeqVariant(VariantID vid);
+  void runOpenMPVariant(VariantID vid);
   void runCudaVariant(VariantID vid);
   void runOpenMPTargetVariant(VariantID vid);
 
