9f987a16fa19e0bd507289cbf245f288de4c915f
Brian Homerding <bhomerding@anl.gov>
2020-12-16 20:01:25 +0000

    minor daxpy update

--- src/basic/DAXPY-Sycl.cpp
+++ src/basic/DAXPY-Sycl.cpp
@@ -75,7 +75,6 @@ void DAXPY::runSyclVariant(VariantID vid)
 
     qu->wait(); // Wait for computation to finish before stopping timer
     stopTimer();
-
     DAXPY_DATA_TEARDOWN_SYCL;
 
   } else if ( vid == RAJA_SYCL ) {
