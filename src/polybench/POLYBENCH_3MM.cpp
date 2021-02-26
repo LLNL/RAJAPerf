afbae9897af7e2392d04e2b25f029ecbcf701e32
Jason Burmark <burmark1@llnl.gov>
2021-02-26 10:46:38 -0800

    Add Lambda GPU variants to some polybench kernels

--- src/polybench/POLYBENCH_3MM.cpp
+++ src/polybench/POLYBENCH_3MM.cpp
@@ -12,12 +12,12 @@
 #include "common/DataUtils.hpp"
 
 
-namespace rajaperf 
+namespace rajaperf
 {
 namespace polybench
 {
 
-  
+
 POLYBENCH_3MM::POLYBENCH_3MM(const RunParams& params)
   : KernelBase(rajaperf::Polybench_3MM, params)
 {
@@ -55,22 +55,24 @@ POLYBENCH_3MM::POLYBENCH_3MM(const RunParams& params)
   setVariantDefined( Base_Seq );
   setVariantDefined( Lambda_Seq );
   setVariantDefined( RAJA_Seq );
-                     
+
   setVariantDefined( Base_OpenMP );
   setVariantDefined( Lambda_OpenMP );
   setVariantDefined( RAJA_OpenMP );
-  
+
   setVariantDefined( Base_OpenMPTarget );
   setVariantDefined( RAJA_OpenMPTarget );
-      
+
   setVariantDefined( Base_CUDA );
+  setVariantDefined( Lambda_CUDA );
   setVariantDefined( RAJA_CUDA );
-        
+
   setVariantDefined( Base_HIP );
+  setVariantDefined( Lambda_HIP );
   setVariantDefined( RAJA_HIP );
 }
 
-POLYBENCH_3MM::~POLYBENCH_3MM() 
+POLYBENCH_3MM::~POLYBENCH_3MM()
 {
 }
 
