1ca2a3caeae7be179b3281ba45b02cfa71f5f7c1
Amy Powell <ajpowel@sandia.gov>
2021-03-02 14:32:51 -0800

    INIT3 with Kokkos Views and custom types

--- src/basic-kokkos/INIT3-Kokkos.cpp
+++ src/basic-kokkos/INIT3-Kokkos.cpp
@@ -26,6 +26,20 @@ void INIT3::runKokkosVariant(VariantID vid)
   
   INIT3_DATA_SETUP;
 
+  // Instantiating Views using getViewFromPointer for the INIT3 definition
+  // (i.e., INIT3.hpp)
+  
+  // The pointer is the first argument, and the last index, denoted by iend, is
+  // your second argument
+  //
+  auto out1_view = getViewFromPointer(out1, iend);
+  auto out2_view = getViewFromPointer(out2, iend);
+  auto out3_view = getViewFromPointer(out3, iend);
+  auto in1_view  = getViewFromPointer(in1, iend);
+  auto in2_view  = getViewFromPointer(in2, iend);
+
+  // Next step, integrate the INIT3_BODY into the Kokkos parallel expression
+
   auto init3_lam = [=](Index_type i) {
                      INIT3_BODY;
                    };
@@ -70,16 +84,23 @@ void INIT3::runKokkosVariant(VariantID vid)
 // Nota bene -- Conversion of Raja code begins here
     case Kokkos_Lambda : {
 
+      Kokkos::fence();
       startTimer();
       for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
 
 //        RAJA::forall<RAJA::simd_exec>(
 //          RAJA::RangeSegment(ibegin, iend), init3_lam);
           
-         // Kokkos translation
-        Kokkos::parallel_for("INIT3-KokkosSeq Kokkos_Lambda", Kokkos::RangePolicy<Kokkos::Serial>(ibegin, iend),
-		[=] (Index_type i) {INIT3_BODY});
+         // Kokkos translation making INIT3_BODY explicit
+        Kokkos::parallel_for("INIT3-Kokkos Kokkos_Lambda", 
+                              Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(ibegin, iend),
+		                      KOKKOS_LAMBDA(Index_type i) {
+                              //INIT3_BODY definition:
+                              // out1[i] = out2[i] = out3[i] = - in1[i] - in2[i] ;
+                                out1_view[i] = out2_view[i] = out3_view[i] = - in1_view[i] - in2_view[i];
+                                });
       }
+      Kokkos::fence();
       stopTimer();
 
       break;
@@ -93,6 +114,15 @@ void INIT3::runKokkosVariant(VariantID vid)
   }
 
 #endif // RUN_KOKKOS
+
+   moveDataToHostFromKokkosView(out1, out1_view, iend);
+   moveDataToHostFromKokkosView(out2, out2_view, iend);
+   moveDataToHostFromKokkosView(out3, out3_view, iend);
+   moveDataToHostFromKokkosView(in1, in1_view, iend);
+   moveDataToHostFromKokkosView(in2, in2_view, iend);
+
+
+
 
 }
 
