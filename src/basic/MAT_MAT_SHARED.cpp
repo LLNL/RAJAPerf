cf91cd8ed6fe1b9a78a02f6381236429ede533d4
Arturo Vargas <vargas45@llnl.gov>
2021-04-20 19:20:11 -0700

    formatting

--- src/basic/MAT_MAT_SHARED.cpp
+++ src/basic/MAT_MAT_SHARED.cpp
@@ -18,26 +18,26 @@ namespace basic {
 MAT_MAT_SHARED::MAT_MAT_SHARED(const RunParams &params)
     : KernelBase(rajaperf::Basic_MAT_MAT_SHARED, params) {
   setDefaultSize(1000);
-  setDefaultReps(5);
+  setDefaultReps(50);
 
   setVariantDefined(Base_Seq);
   setVariantDefined( Lambda_Seq );
   setVariantDefined( RAJA_Seq );
 
-  // setVariantDefined( Base_OpenMP );
-  //setVariantDefined( Lambda_OpenMP );
-  // setVariantDefined( RAJA_OpenMP );
+  setVariantDefined( Base_OpenMP );
+  setVariantDefined( Lambda_OpenMP );
+  setVariantDefined( RAJA_OpenMP );
 
   // setVariantDefined( Base_OpenMPTarget );
   // setVariantDefined( RAJA_OpenMPTarget );
 
-  // setVariantDefined( Base_CUDA );
-  // setVariantDefined( Lambda_CUDA );
-  // setVariantDefined( RAJA_CUDA );
+  setVariantDefined( Base_CUDA );
+  setVariantDefined( Lambda_CUDA );
+  setVariantDefined( RAJA_CUDA );
 
-  // setVariantDefined( Base_HIP );
-  // setVariantDefined( Lambda_HIP );
-  // setVariantDefined( RAJA_HIP );
+  setVariantDefined( Base_HIP );
+  setVariantDefined( Lambda_HIP );
+  setVariantDefined( RAJA_HIP );
 }
 
 MAT_MAT_SHARED::~MAT_MAT_SHARED() {}
