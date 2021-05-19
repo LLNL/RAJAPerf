13065efd03217c794ad3b4baa30efca5bccfc26c
Rich Hornung <hornung1@llnl.gov>
2021-05-19 10:29:26 -0700

    removed unnecessary namespacing in exec poilicy name.

--- src/stream/DOT-OMPTarget.cpp
+++ src/stream/DOT-OMPTarget.cpp
@@ -77,7 +77,7 @@ void DOT::runOpenMPTargetVariant(VariantID vid)
 
       RAJA::ReduceSum<RAJA::omp_target_reduce, Real_type> dot(m_dot_init);
 
-      RAJA::forall<RAJA::policy::omp::omp_target_parallel_for_exec<threads_per_team>>(
+      RAJA::forall<RAJA::omp_target_parallel_for_exec<threads_per_team>>(
           RAJA::RangeSegment(ibegin, iend), [=](Index_type i) {
         DOT_BODY;
       });
