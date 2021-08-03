f4fc145e2e43e976e7b330443a23eb403ec10ef0
Jeff Hammond <jeff.science@gmail.com>
2021-08-03 11:45:12 +0300

    EOS

--- src/lcals/EOS-StdPar.cpp
+++ src/lcals/EOS-StdPar.cpp
@@ -10,6 +10,10 @@
 
 #include "RAJA/RAJA.hpp"
 
+#include <ranges>
+#include <algorithm>
+#include <execution>
+
 #include <iostream>
 
 namespace rajaperf 
@@ -20,6 +24,8 @@ namespace lcals
 
 void EOS::runStdParVariant(VariantID vid)
 {
+#if defined(RUN_STDPAR)
+
   const Index_type run_reps = getRunReps();
   const Index_type ibegin = 0;
   const Index_type iend = getActualProblemSize();
@@ -34,13 +40,35 @@ void EOS::runStdParVariant(VariantID vid)
 
     case Base_StdPar : {
 
+      auto range = std::views::iota(ibegin, iend);
+
       startTimer();
       for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
 
-        for (Index_type i = ibegin; i < iend; ++i ) {
+        std::for_each( std::execution::par_unseq,
+                        std::begin(range), std::end(range),
+                        [=](Index_type i) {
           EOS_BODY;
-        }
+        });
 
+      }
+      stopTimer();
+
+      break;
+    }
+
+    case Lambda_StdPar : {
+
+      auto range = std::views::iota(ibegin, iend);
+
+      startTimer();
+      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
+
+        std::for_each( std::execution::par_unseq,
+                        std::begin(range), std::end(range),
+                        [=](Index_type i) {
+          eos_lam(i);
+        });
       }
       stopTimer();
 
@@ -48,21 +76,6 @@ void EOS::runStdParVariant(VariantID vid)
     }
 
 #if defined(RUN_RAJA_STDPAR)
-    case Lambda_StdPar : {
-
-      startTimer();
-      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
-
-        for (Index_type i = ibegin; i < iend; ++i ) {
-          eos_lam(i);
-        }
-
-      }
-      stopTimer();
-
-      break;
-    }
-
     case RAJA_StdPar : {
 
       startTimer();
@@ -84,6 +97,7 @@ void EOS::runStdParVariant(VariantID vid)
 
   }
 
+#endif
 }
 
 } // end namespace lcals
