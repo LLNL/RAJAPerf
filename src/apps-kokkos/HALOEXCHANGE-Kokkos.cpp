ff48c3a3a96b0d6c8aeb67a539bbf49a1a996415
Amy Powell <ajpowel@sandia.gov>
2021-10-08 14:20:07 -0600

    HALOEXCHANGE-Kokkos.cpp: Kokkos translation

--- src/apps-kokkos/HALOEXCHANGE-Kokkos.cpp
+++ src/apps-kokkos/HALOEXCHANGE-Kokkos.cpp
@@ -24,92 +24,54 @@ void HALOEXCHANGE::runKokkosVariant(VariantID vid)
         //return;
 
   const Index_type run_reps = getRunReps();
+  // Nota bene: ibegin, iend not defined for this kernel
+  // Instead:
+  // Index_type num_neighbors = s_num_neighbors;
+  // Index_type num_vars = m_num_vars; 
+  // How these variables are set::
+  // apps/HALOEXCHANGE.cpp:  m_num_vars_default     = 3;
+  // apps/HALOEXCHANGE.hpp:  static const int s_num_neighbors = 26;
 
-  HALOEXCHANGE_DATA_SETUP;
+  //HALOEXCHANGE_DATA_SETUP;
+
+// Declare and define Kokkos Views
+// Preserving the names of the pointer variables to avoid typo errors in the
+// Kokkos_Lambda expressions
+//
+//auto vars_view = getViewFromPointer(vars, num_neighbors);
+//auto buffers_view = getViewFromPointer(buffers, num_neighbors);
+
+std::vector<Kokkos::View<Real_ptr>> vars; 
+std::vector<Kokkos::View<Real_ptr>> buffers; 
+std::vector<Kokkos::View<Int_ptr>>  pack_index_lists; 
+std::vector<Kokkos::View<Int_ptr>>  unpack_index_lists; 
+
+for (auto var: m_vars) {
+	vars.push_back(getViewFromPointer(var, m_var_size));
+}
+
+for ( int x = 0; x < m_buffers.size(); ++x ) {
+    Index_type buffer_len = m_num_vars * m_pack_index_list_lengths[x];
+	buffers.push_back(getViewFromPointer(m_buffers[x], buffer_len));
+}
+
+
+for ( int x = 0; x < m_pack_index_lists.size(); ++x ) {
+
+	pack_index_lists.push_back(getViewFromPointer(m_pack_index_lists[x], m_pack_index_list_lengths[x]));
+}
+
+
+for ( int x = 0; x < m_unpack_index_lists.size(); ++x ) {
+
+	unpack_index_lists.push_back(getViewFromPointer(m_unpack_index_lists[x], m_unpack_index_list_lengths[x]));
+}
+auto num_neighbors = s_num_neighbors;
+auto num_vars = m_num_vars;
 
 #if defined(RUN_KOKKOS)
 
   switch ( vid ) {
-
-    case Base_Seq : {
-
-      startTimer();
-      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
-
-        for (Index_type l = 0; l < num_neighbors; ++l) {
-          Real_ptr buffer = buffers[l];
-          Int_ptr list = pack_index_lists[l];
-          Index_type  len  = pack_index_list_lengths[l];
-          for (Index_type v = 0; v < num_vars; ++v) {
-            Real_ptr var = vars[v];
-            for (Index_type i = 0; i < len; i++) {
-              HALOEXCHANGE_PACK_BODY;
-            }
-            buffer += len;
-          }
-        }
-
-        for (Index_type l = 0; l < num_neighbors; ++l) {
-          Real_ptr buffer = buffers[l];
-          Int_ptr list = unpack_index_lists[l];
-          Index_type  len  = unpack_index_list_lengths[l];
-          for (Index_type v = 0; v < num_vars; ++v) {
-            Real_ptr var = vars[v];
-            for (Index_type i = 0; i < len; i++) {
-              HALOEXCHANGE_UNPACK_BODY;
-            }
-            buffer += len;
-          }
-        }
-
-      }
-      stopTimer();
-
-      break;
-    }
-
-    case Lambda_Seq : {
-
-      startTimer();
-      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
-
-        for (Index_type l = 0; l < num_neighbors; ++l) {
-          Real_ptr buffer = buffers[l];
-          Int_ptr list = pack_index_lists[l];
-          Index_type  len  = pack_index_list_lengths[l];
-          for (Index_type v = 0; v < num_vars; ++v) {
-            Real_ptr var = vars[v];
-            auto haloexchange_pack_base_lam = [=](Index_type i) {
-                  HALOEXCHANGE_PACK_BODY;
-                };
-            for (Index_type i = 0; i < len; i++) {
-              haloexchange_pack_base_lam(i);
-            }
-            buffer += len;
-          }
-        }
-
-        for (Index_type l = 0; l < num_neighbors; ++l) {
-          Real_ptr buffer = buffers[l];
-          Int_ptr list = unpack_index_lists[l];
-          Index_type  len  = unpack_index_list_lengths[l];
-          for (Index_type v = 0; v < num_vars; ++v) {
-            Real_ptr var = vars[v];
-            auto haloexchange_unpack_base_lam = [=](Index_type i) {
-                  HALOEXCHANGE_UNPACK_BODY;
-                };
-            for (Index_type i = 0; i < len; i++) {
-              haloexchange_unpack_base_lam(i);
-            }
-            buffer += len;
-          }
-        }
-
-      }
-      stopTimer();
-
-      break;
-    }
 /*
     case RAJA_Seq : {
 
@@ -163,38 +125,54 @@ void HALOEXCHANGE::runKokkosVariant(VariantID vid)
       startTimer();
 
       for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
-
+        // FYI: num_neigbors defined in HALOEXCHANGE.hpp
+        // num_neighbors is set in HALOEXCHANGE.cpp
         for (Index_type l = 0; l < num_neighbors; ++l) {
-          Real_ptr buffer = buffers[l];
-          Int_ptr list = pack_index_lists[l];
-          Index_type  len  = pack_index_list_lengths[l];
+          auto buffer = buffers[l];
+          auto list = pack_index_lists[l];
+          Index_type  len  = m_pack_index_list_lengths[l];
+        // FYI: num_vars defined in HALOEXCHANGE.hpp
+        // num_vars is set in HALOEXCHANGE.cpp
           for (Index_type v = 0; v < num_vars; ++v) {
-            Real_ptr var = vars[v];
+            auto var = vars[v];
             auto haloexchange_pack_base_lam = KOKKOS_LAMBDA(Index_type i) {
-                  HALOEXCHANGE_PACK_BODY;
+                  // HALOEXCHANGE_PACK_BODY
+                  // #define HALOEXCHANGE_PACK_BODY \
+                  // buffer[i] = var[list[i]];
+                   buffer[i] = var[list[i]];
                 };
 
 Kokkos::parallel_for("HALOEXCHANGE - Pack Body - Kokkos Lambda",
 			         Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(0, len),
                      haloexchange_pack_base_lam);
-            buffer += len;
+            //buffer += len
+            
+            auto end = buffer.extent(0);
+            decltype(end) begin = len;
+            buffer = Kokkos::subview(buffer, std::make_pair(begin,end));
           }
         }
 
         for (Index_type l = 0; l < num_neighbors; ++l) {
-          Real_ptr buffer = buffers[l];
-          Int_ptr list = unpack_index_lists[l];
-          Index_type  len  = unpack_index_list_lengths[l];
+          auto buffer = buffers[l];
+          auto list = unpack_index_lists[l];
+          Index_type  len  = m_unpack_index_list_lengths[l];
           for (Index_type v = 0; v < num_vars; ++v) {
-            Real_ptr var = vars[v];
+            auto var = vars[v];
             auto haloexchange_unpack_base_lam = KOKKOS_LAMBDA(Index_type i) {
-                  HALOEXCHANGE_UNPACK_BODY;
+				//#define HALOEXCHANGE_UNPACK_BODY \
+  				//var[list[i]] = buffer[i];
+  				var[list[i]] = buffer[i];
+
                 };
 
             Kokkos::parallel_for("HALOEXCHANGE - Unpack Body - Kokkos Lambda",
 			         Kokkos::RangePolicy<Kokkos::DefaultExecutionSpace>(0, len),
                      haloexchange_unpack_base_lam);
-            buffer += len;
+            //buffer += len;
+            auto end = buffer.extent(0);
+            decltype(end) begin = len;
+            buffer = Kokkos::subview(buffer, std::make_pair(begin,end));
           }
         }
 
@@ -211,8 +189,34 @@ Kokkos::parallel_for("HALOEXCHANGE - Pack Body - Kokkos Lambda",
   }
 
 #endif // RUN_KOKKOS
+  //Usage:  moveDataToHostFromKokkosView(pointer, pointer_wrapped_view, iend);
+  // moveDataToHostFromKokkosView(vars, vars_view, num_neighbors);
+  // moveDataToHostFromKokkosView(buffers, buffers_view, num_neighbors);
 
 
+for ( int x = 0; x < m_vars.size(); ++x ) {
+	//vars.push_back(getViewFromPointer(var, m_var_size));
+    moveDataToHostFromKokkosView(m_vars[x], vars[x], m_var_size);
+}
+
+for ( int x = 0; x < m_buffers.size(); ++x ) {
+    Index_type buffer_len = m_num_vars * m_pack_index_list_lengths[x];
+    moveDataToHostFromKokkosView(m_buffers[x], buffers[x], buffer_len);
+}
+
+
+for ( int x = 0; x < m_pack_index_lists.size(); ++x ) {
+
+	//pack_index_lists.push_back(getViewFromPointer(m_pack_index_lists[x], m_pack_index_list_lengths[x]));
+    moveDataToHostFromKokkosView(m_pack_index_lists[x], pack_index_lists[x], m_pack_index_list_lengths[x]);
+}
+
+
+for ( int x = 0; x < m_unpack_index_lists.size(); ++x ) {
+
+	//unpack_index_lists.push_back(getViewFromPointer(m_unpack_index_lists[x], m_unpack_index_list_lengths[x]));
+    moveDataToHostFromKokkosView(m_unpack_index_lists[x], unpack_index_lists[x], m_unpack_index_list_lengths[x]);
+}
 
 }
 
