936f19accddb82b6a98a28272c3bc502befa119b
Brian Homerding <bhomerding@anl.gov>
2021-10-11 21:45:47 +0000

    SYCL DOT variants

--- src/stream/DOT.cpp
+++ src/stream/DOT.cpp
@@ -53,8 +53,8 @@ DOT::DOT(const RunParams& params)
   setVariantDefined( Base_HIP );
   setVariantDefined( RAJA_HIP );
 
-//  setVariantDefined( Base_SYCL );
-//  setVariantDefined( RAJA_SYCL );
+  setVariantDefined( Base_SYCL );
+  setVariantDefined( RAJA_SYCL );
 }
 
 DOT::~DOT()
