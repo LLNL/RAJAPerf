98dc7fd3360478eebe60aecce18f3a1e1d65ca7a
Corbin Robeck <corbin.robeck@amd.com>
2021-10-13 09:33:02 -0500

    updating paths

--- scripts/kernel_parameter_sweep.py
+++ scripts/kernel_parameter_sweep.py
@@ -5,12 +5,9 @@ import sys
 
 
 if __name__ == '__main__':
-    #block_sizes = [64, 128, 256, 512, 1024]
-    #grid_sizes = [2,4,8,16,32,64,128,256,512,1024,2**11,2**12,2**13,2**14,2**15,2**16,2**17,2**18,2**19,2**20]
-    #problem_sizes = [2**11,2**12,2**13,2**14,2**15,2**16,2**17,2**18,2**19,2**20]
     block_sizes = [64, 128, 256, 512, 1024]
-    grid_sizes = [2,4,8,16,32,64,128]
-    problem_sizes = [2**11]
+    grid_sizes = [2,4,8,16,32,64,128,256,512,1024,2**11,2**12,2**13,2**14,2**15,2**16,2**17,2**18,2**19,2**20]
+    problem_sizes = [2**11,2**12,2**13,2**14,2**15,2**16,2**17,2**18,2**19,2**20]
 
     sizeof = 4.0 #floats
     checksum_threshold = 1.0e-6
@@ -21,6 +18,7 @@ if __name__ == '__main__':
     data_array = np.array([], dtype=np.int64).reshape(0,6)
     
     top_path = os.path.abspath(os.path.join(__file__ ,"../.."))
+    script_path = os.path.abspath(os.path.join(__file__ ,".."))
     build_path = top_path + "/build"
     src_path = top_path + "/src/apps"
     test_file = "PRESSURE-Hip.cpp"
@@ -37,12 +35,12 @@ if __name__ == '__main__':
 
     for grid_size in grid_sizes:
         for line in fileinput.input([src_path+"/"+test_file], inplace=True):
-           if line.startswith("       const size_t grid_size"):
+           if "const size_t grid_size" in line:
                line = '       const size_t grid_size = '+ str(grid_size) + ';\n'
            sys.stdout.write(line)
         for block_size in block_sizes:
             for line in fileinput.input([src_path+"/"+test_file], inplace=True):
-               if line.startswith("  const size_t block_size"):
+               if "const size_t block_size" in line:
                    line = '  const size_t block_size = '+ str(block_size) + ';\n'
                sys.stdout.write(line)                   
             #Recompile
@@ -50,7 +48,6 @@ if __name__ == '__main__':
             os.system(make_cmd)
             os.chdir(exe_path)
             for problem_size in problem_sizes:
-    #            grid_size = -(-problem_size // block_size)
                 print(block_size,grid_size,problem_size)
                 os.system(cmd + " " + str(problem_size))
                 with open(checksum_file, 'r') as f:
@@ -70,6 +67,6 @@ if __name__ == '__main__':
     
     print(header)
     print(data_array)
-    os.chdir(top_path)
+    os.chdir(script_path)
     np.savetxt("data.csv",data_array, header=header, fmt='%4d, %4d, %4d, %10.5f, %10.5f, %10.5f')
     print("Kernel parameter sweep results in data.csv")
