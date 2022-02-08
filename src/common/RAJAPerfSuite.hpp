f6b8232fb51d4d95e788b3ca56f2523a6207c7a6
Amy Powell <ajpowel@.gov>
2022-02-07 17:15:07 -0700

    Delete code now in KokkosViewUtils.hpp

--- src/common/RAJAPerfSuite.hpp
+++ src/common/RAJAPerfSuite.hpp
@@ -16,7 +16,6 @@
 #include "RAJA/config.hpp"
 #include "rajaperf_config.hpp"
 
-
 #if defined(RUN_KOKKOS)
 #include "Kokkos_Core.hpp"
 #endif // RUN_KOKKOS
@@ -399,52 +398,6 @@ auto getViewFromPointer(PointedAt *kokkos_ptr, Boundaries... boundaries)
   // An allocation is implicitly made here
   device_view_type device_data_copy("StringName", boundaries...);
 
-  mirror_view_type cpu_to_gpu_mirror =
-      Kokkos::create_mirror_view(device_data_copy);
-
-
-  Kokkos::deep_copy(cpu_to_gpu_mirror, pointer_holder);
-
-  Kokkos::deep_copy(device_data_copy, cpu_to_gpu_mirror);
-
-  // Kokkos::View return type
-
-  return device_data_copy;
-}
-
-// This function will move data in a Kokkos::View back to host from device,
-// and will be stored in the existing pointer(s)
-template <class PointedAt, class ExistingView, class... Boundaries>
-void moveDataToHostFromKokkosView(PointedAt *kokkos_ptr, ExistingView my_view,
-                                  Boundaries... boundaries)
-{
-
-  using host_view_type = typename Kokkos::View<
-      typename PointerOfNdimensions<PointedAt, sizeof...(Boundaries)>::type,
-      typename Kokkos::DefaultHostExecutionSpace::memory_space>;
-
-  using device_view_type = typename Kokkos::View<
-      typename PointerOfNdimensions<PointedAt, sizeof...(Boundaries)>::type,
-      typename Kokkos::DefaultExecutionSpace::memory_space>;
-
-  using mirror_view_type = typename device_view_type::HostMirror;
-
-
-  host_view_type pointer_holder(kokkos_ptr, boundaries...);
-
-  // Layout is optimal for gpu, but data are actually located on CPU
-  mirror_view_type cpu_to_gpu_mirror = Kokkos::create_mirror_view(my_view);
-
-  // Actual copying of the data from the gpu (my_view) back to the cpu
-  Kokkos::deep_copy(cpu_to_gpu_mirror, my_view);
-
-  // This copies from the mirror on the host cpu back to the existing
-  // pointer(s)
-  Kokkos::deep_copy(pointer_holder, cpu_to_gpu_mirror);
-}
-
-#endif // RUN_KOKKOS
-*/
 } // namespace rajaperf
 
 #endif // closing endif for header file include guard
