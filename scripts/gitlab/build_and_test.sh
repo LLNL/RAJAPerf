16fab4270a4cd4b679549d3d1af400eea7e0e881
Rich Hornung <hornung1@llnl.gov>
2022-03-17 15:20:02 -0700

    mv host-config to top-level project directory

--- scripts/gitlab/build_and_test.sh
+++ scripts/gitlab/build_and_test.sh
@@ -58,6 +58,8 @@ then
 
     python3 tpl/RAJA/scripts/uberenv/uberenv.py --project-json=".uberenv_config.json" --spec="${spec}" ${prefix_opt}
 
+    mv ${project_dir}/tpl/RAJA/hc-*.cmake ${project_dir}/.
+
 fi
 date
 
@@ -66,7 +68,7 @@ if [[ -z ${hostconfig} ]]
 then
     # If no host config file was provided, we assume it was generated.
     # This means we are looking of a unique one in project dir.
-    hostconfigs=( $( ls "${project_dir}/tpl/RAJA/"hc-*.cmake ) )
+    hostconfigs=( $( ls "${project_dir}/"hc-*.cmake ) )
     if [[ ${#hostconfigs[@]} == 1 ]]
     then
         hostconfig_path=${hostconfigs[0]}
