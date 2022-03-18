6a70a6525d7f2083761528d8dc5c2a65af962142
Jason Burmark <burmark1@llnl.gov>
2022-03-17 17:04:20 -0700

    Fix unused var varning in MASS3DPA

--- src/apps/MASS3DPA-Hip.cpp
+++ src/apps/MASS3DPA-Hip.cpp
@@ -102,7 +102,7 @@ __global__ void Mass3DPA(Index_type NE, const Real_ptr B, const Real_ptr Bt,
   }
 }
 
-void MASS3DPA::runHipVariant(VariantID vid, size_t tune_idx) {
+void MASS3DPA::runHipVariant(VariantID vid, size_t /*tune_idx*/) {
   const Index_type run_reps = getRunReps();
 
   MASS3DPA_DATA_SETUP;
