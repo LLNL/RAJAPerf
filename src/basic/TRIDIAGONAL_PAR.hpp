b51b662a9013a7ff568c595dd2f7e2543a424cd8
Jason Burmark <burmark1@llnl.gov>
2022-03-22 13:18:13 -0700

    Add tridiagonal par reference impl

--- src/basic/TRIDIAGONAL_PAR.hpp
+++ src/basic/TRIDIAGONAL_PAR.hpp
@@ -7,34 +7,128 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 ///
-/// Tri-Diagonal matrix solver
+/// Tri-Diagonal parallel matrix solver
 /// reference implementation:
 ///
-///      Real_ptr d = new Real_type[N];
-///
-///      for (Index_type i = 0; i < Nelements; ++i) {
-///
-///        Real_ptr Aa = Aa_global + i * N;
-///        Real_ptr Ab = Ab_global + i * N;
-///        Real_ptr Ac = Ac_global + i * N;
-///        Real_ptr x = x_global + i * N;
-///        Real_ptr b = b_global + i * N;
-///
-///        d[0] = Ac[0] / Ab[0];
-///        x[0] =  b[0] / Ab[0];
-///        for (Index_type n = 1; n < N; ++n) {
-///          Real_type div = Ab[n] - Aa[n] * d[n-1];
-///          d[n] = Ac[n] / div;
-///          x[n] = (b[n] - Aa[n] * x[n-1]) / div;
-///        }
-///
-///        for (Index_type n = N-2; n >= 0; --n) {
-///          x[n] = x[n] - d[n] * x[n+1];
-///        }
-///
+///      Real_type E_[N-1]; // lower diagonal of A
+///      Real_ptr E = E_ - 2; // [2:N]
+///      for (int j = 2; j <= N; ++j) { // par
+///        E[j] = Aa[j-1];
+///      }
+///      Real_type F_[N-1]; // upper diagonal of A
+///      Real_ptr F = F_ - 1; // [1:N-1]
+///      for (int j = 1; j <= N-1; ++j) { // par
+///        F[j] = Ac[j-1];
+///      }
+///      Real_type D_[N]; // diagonal of A
+///      Real_ptr D = D_ - 1; // [1:N]
+///      for (int j = 1; j <= N; ++j) { // par
+///        D[j] = Ab[j-1];
+///      }
+///      Real_type B_[N]; // rhs of equation
+///      Real_ptr B = B_ - 1; // [1:N]
+///      for (int j = 1; j <= N; ++j) { // par
+///        B[j] = b[j-1];
 ///      }
 ///
-///      delete[] d;
+///      Real_type EF_[N]; // holds products (-e[i]*f[i-1])
+///      Real_ptr EF = EF_ - 1; // [1:N]
+///      Real_type TEMP_[N]; // temporary array
+///      Real_ptr TEMP = TEMP_ - 1; // [1:N]
+///      Real_type QI_[N]; // Qi[j]
+///      Real_ptr QI = QI_ - 1; // [1:N]
+///      Real_type QIM1_[N+1]; // Qi-1[j]
+///      Real_ptr QIM1 = QIM1_ - 0; // [0:N]
+///      Real_type QIM2_[N+2]; // Qi-1[j]
+///      Real_ptr QIM2 = QIM2_ - (-1); // [-1:N]
+///
+///      Real_type U_[N];
+///      Real_ptr U = U_ - 1; // [1:N]
+///
+///      // Real_type M_[N-1];
+///      // Real_ptr M = M_ - 2; // [2:N]
+///      Real_type M_[N];
+///      Real_ptr M = M_ - 1; // [1:N]
+///
+///      Real_type Y_[N];
+///      Real_ptr Y = Y_ - 1; // [1:N]
+///
+///      Real_type X_[N];
+///      Real_ptr X = X_ - 1; // [1:N]
+///
+///      EF[1] = 0;
+///      for (int j = 2; j <= N; ++j) { // par
+///        EF[j] = -E[j] * F[j-1];
+///      }
+///      for (int j = -1; j <= N; ++j) { // par
+///        QIM2[j] = 1;
+///      }
+///      QIM1[0] = 1;
+///      for (int j = 1; j <= N; ++j) { // par
+///        QIM1[j] = D[j];
+///      }
+///      QI[1] = D[1];
+///      for (int j = 2; j <= N; ++j) { // par
+///        QI[j] = D[j] * D[j-1] + EF[j];
+///      }
+///      for (int i = 2; i <= N; i *= 2) {
+///        for (int j = i-1; j <= N; ++j) { // par
+///          TEMP[j] = QIM1[j] * QIM1[j-i+1] + EF[j-i+2] * QIM2[j] * QIM2[j-i];
+///        }
+///        for (int j = N; j >= i; --j) { // par (beware)
+///          QIM1[j] = QI[j] * QIM1[j-i] + EF[j-i+1] * QIM1[j] * QIM2[j-i-1];
+///        }
+///        for (int j = i-1; j <= N; ++j) { // par
+///          QIM2[j] = TEMP[j];
+///        }
+///        for (int j = i+1; j <= N; ++j) { // par
+///          QI[j] = D[j] * QIM1[j-1] + EF[j] * QIM2[j-2];
+///        }
+///      }
+///
+///      U[1] = QI[1];
+///      for (int j = 2; j <= N; ++j) { // par
+///        U[j] = QI[j] / QI[j-1];
+///      }
+///      for (int j = 2; j <= N; ++j) { // par
+///        M[j] = E[j] / U[j-1];
+///      }
+///      for (int j = 1; j <= N; ++j) { // par
+///        Y[j] = B[j];
+///      }
+///      M[1] = 0;
+///      for (int j = 2; j <= N; ++j) { // par
+///        M[j] = -M[j];
+///      }
+///
+///      for (int i = 1; i <= N; i *= 2) {
+///        for (int j = N; j >= i+1; --j) { // par (beware)
+///          Y[j] = Y[j] + Y[j-i] * M[j];
+///        }
+///        for (int j = N; j >= i+1; --j) { // par (beware)
+///          M[j] = M[j] * M[j-i];
+///        }
+///      }
+///
+///      for (int j = 1; j <= N; ++j) { // par
+///        X[j] = Y[j] / U[j];
+///      }
+///      for (int j = 1; j <= N-1; ++j) { // par
+///        M[j] = -F[j] / U[j];
+///      }
+///      M[N] = 0;
+///      for (int i = 1; i <= N; i *= 2) {
+///        for (int j = 1; j <= N-i; ++j) { // par (beware)
+///          X[j] = X[j] + X[j+i] * M[j];
+///        }
+///        for (int j = 1; j <= N-i; ++j) { // par (beware)
+///          M[j] = M[j] * M[j+i];
+///        }
+///      }
+///
+///      for (int i = 1; i <= N; ++i) { // par
+///        x[i-1] = X[i];
+///      }
 ///
 
 #ifndef RAJAPerf_Basic_TRIDIAGONAL_PAR_HPP
