87ed7932eda31c0eff0a3b59cf4fa045ac87052a
Corbin Robeck <corbin.robeck@amd.com>
2022-04-08 12:36:20 -0500

    fix incorrect init variables in REDUCE_STRUCT-OMP.cpp

--- src/basic/REDUCE_STRUCT-OMP.cpp
+++ src/basic/REDUCE_STRUCT-OMP.cpp
@@ -81,10 +81,10 @@ void REDUCE_STRUCT::runOpenMPVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(t
                                  reduction(max:ymax)
 
       for (Index_type i = ibegin; i < iend; ++i ) {
-        xsum += reduce_struct_x_base_lam(i);
+        xsum += init_struct_x_base_lam(i);
         xmin = RAJA_MIN(xmin, init_struct_x_base_lam(i));
         xmax = RAJA_MAX(xmax, init_struct_x_base_lam(i));
-        ysum += reduce_struct_y_base_lam(i);
+        ysum += init_struct_y_base_lam(i);
         ymin = RAJA_MIN(ymin, init_struct_y_base_lam(i));
         ymax = RAJA_MAX(ymax, init_struct_y_base_lam(i));
 
