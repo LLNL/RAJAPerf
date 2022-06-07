5169e6e4db3242fa36bbe7092bb84a7a89446f29
Corbin Robeck <corbin.robeck@amd.com>
2022-06-07 09:25:20 -0500

    update unroll pragma to RAJAPERF_UNROLL

--- src/basic/MAT_FUSED_MUL_ADD-Hip.cpp
+++ src/basic/MAT_FUSED_MUL_ADD-Hip.cpp
@@ -63,7 +63,7 @@ __global__ void mat_fused_mul_add_builtin(const Real_ptr A, const Real_ptr B, Re
     b_idx += 4*Ne; // move two rows down
   }
 
-  #pragma unroll 4
+  RAJAPERF_UNROLL(4)
   for(Index_type i = 0; i < 4; ++i){
     const Index_type d_idx =  threadIdx.x
       + Ne * (threadIdx.y + 4 * i); 
