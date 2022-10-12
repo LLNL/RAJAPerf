a884cd16c854f8d43c66fe4d2342534517eecb7d
Jeff Hammond <jehammond@nvidia.com>
2022-10-12 09:01:14 +0300

    fix atomic_ref

--- src/basic/DAXPY_ATOMIC-StdPar.cpp
+++ src/basic/DAXPY_ATOMIC-StdPar.cpp
@@ -15,11 +15,6 @@
 #include <iostream>
 #include <atomic>
 
-#if defined(NVCXX_GPU_ENABLED)
-// this is required to get NVC++ to compile CUDA atomics in StdPar
-#include <openacc.h>
-#endif
-
 namespace rajaperf
 {
 namespace basic
@@ -49,9 +44,9 @@ void DAXPY_ATOMIC::runStdParVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tu
         std::for_each( std::execution::par_unseq,
                        begin, end,
                        [=](Index_type i) {
-#if 0 //__cpp_lib_atomic_ref
-          auto px = std::atomic_ref<Real_type>(&x[i]);
-          auto py = std::atomic_ref<Real_type>(&y[i]);
+#if __cpp_lib_atomic_ref
+          auto px = std::atomic_ref<Real_type>(x[i]);
+          auto py = std::atomic_ref<Real_type>(y[i]);
           py += a * px;
 #elif defined(_OPENMP)
           #pragma omp atomic
@@ -62,7 +57,8 @@ void DAXPY_ATOMIC::runStdParVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tu
 #elif defined(NVCXX_GPU_ENABLED)
           atomicaddd(&y[i],a * x[i]);
 #else
-#error No atomic
+#warning No atomic
+          y[i] += a * x[i];
 #endif
         });
 
@@ -75,9 +71,23 @@ void DAXPY_ATOMIC::runStdParVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tu
     case Lambda_StdPar : {
 
       auto daxpy_atomic_lam = [=](Index_type i) {
-                         #pragma omp atomic
-                         y[i] += a * x[i] ;
-                       };
+#if __cpp_lib_atomic_ref
+          auto px = std::atomic_ref<Real_type>(x[i]);
+          auto py = std::atomic_ref<Real_type>(y[i]);
+          py += a * px;
+#elif defined(_OPENMP)
+          #pragma omp atomic
+          y[i] += a * x[i];
+#elif defined(_OPENACC)
+          #pragma acc atomic
+          y[i] += a * x[i];
+#elif defined(NVCXX_GPU_ENABLED)
+          atomicaddd(&y[i],a * x[i]);
+#else
+#warning No atomic
+          y[i] += a * x[i];
+#endif
+      };
 
       startTimer();
       for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
