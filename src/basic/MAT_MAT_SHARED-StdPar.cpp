c59a972bacc57fe7872f1fc7b1a7fd34bb821a3b
Jeff Hammond <jeff.science@gmail.com>
2022-10-13 13:15:53 +0300

    need empty impl after all

---
+++ src/basic/MAT_MAT_SHARED-StdPar.cpp
@@ -0,0 +1,25 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2017-20, Lawrence Livermore National Security, LLC
+// and RAJA Performance Suite project contributors.
+// See the RAJAPerf/LICENSE file for details.
+//
+// SPDX-License-Identifier: (BSD-3-Clause)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#include "MAT_MAT_SHARED.hpp"
+
+#include <iostream>
+
+namespace rajaperf {
+namespace basic {
+
+void MAT_MAT_SHARED::runStdParVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune_idx))
+{
+  default: {
+    getCout() << "\n  MAT_MAT_SHARED : Unknown variant id = " << vid
+              << std::endl;
+  }
+}
+
+} // end namespace basic
+} // end namespace rajaperf
