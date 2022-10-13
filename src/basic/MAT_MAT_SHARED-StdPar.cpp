83ccc76dbd119c14153b2932abaa5e71d02c71f0
Jeff Hammond <jeff.science@gmail.com>
2022-10-13 13:24:01 +0300

    need empty impl after all

--- src/basic/MAT_MAT_SHARED-StdPar.cpp
+++ src/basic/MAT_MAT_SHARED-StdPar.cpp
@@ -15,9 +15,12 @@ namespace basic {
 
 void MAT_MAT_SHARED::runStdParVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune_idx))
 {
+
+  switch (vid) {
   default: {
     getCout() << "\n  MAT_MAT_SHARED : Unknown variant id = " << vid
               << std::endl;
+  }
   }
 }
 
