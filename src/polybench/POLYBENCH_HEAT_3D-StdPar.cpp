456e72285261096db7ff243a4441c31011aae393
Jeff Hammond <jeff.science@gmail.com>
2022-10-13 09:35:06 +0300

    for_each_n

--- src/polybench/POLYBENCH_HEAT_3D-StdPar.cpp
+++ src/polybench/POLYBENCH_HEAT_3D-StdPar.cpp
@@ -29,11 +29,6 @@ void POLYBENCH_HEAT_3D::runStdParVariant(VariantID vid, size_t tune_idx)
 
 #ifdef USE_STDPAR_COLLAPSE
   const auto nn = N-2;
-  counting_iterator<Index_type> begin(0);
-  counting_iterator<Index_type> end(nn*nn*nn);
-#else
-  counting_iterator<Index_type> begin(1);
-  counting_iterator<Index_type> end(N-1);
 #endif
 
   switch ( vid ) {
@@ -46,23 +41,23 @@ void POLYBENCH_HEAT_3D::runStdParVariant(VariantID vid, size_t tune_idx)
         for (Index_type t = 0; t < tsteps; ++t) { 
 
 #ifdef USE_STDPAR_COLLAPSE
-        std::for_each( std::execution::par_unseq,
-                        begin, end,
-                        [=](Index_type ijk) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), nn*nn*nn,
+                         [=](Index_type ijk) {
               const auto i  = 1 + ijk / (nn*nn);
               const auto jk = ijk % (nn*nn);
               const auto j  = 1 + jk / nn;
               const auto k  = 1 + jk % nn;
 #else
-          std::for_each( std::execution::par_unseq,
-                         begin, end,
-                         [=](Index_type i) {
-            std::for_each( std::execution::unseq,
-                           begin, end,
-                           [=](Index_type j) {
-              std::for_each( std::execution::unseq,
-                           begin, end,
-                           [=](Index_type k) {
+          std::for_each_n( std::execution::par_unseq,
+                           counting_iterator<Index_type>(1), N-2,
+                           [=](Index_type i) {
+            std::for_each_n( std::execution::unseq,
+                             counting_iterator<Index_type>(1), N-2,
+                             [=](Index_type j) {
+              std::for_each_n( std::execution::unseq,
+                               counting_iterator<Index_type>(1), N-2,
+                               [=](Index_type k) {
 #endif
                 POLYBENCH_HEAT_3D_BODY1;
 #ifndef USE_STDPAR_COLLAPSE
@@ -72,23 +67,23 @@ void POLYBENCH_HEAT_3D::runStdParVariant(VariantID vid, size_t tune_idx)
           });
 
 #ifdef USE_STDPAR_COLLAPSE
-        std::for_each( std::execution::par_unseq,
-                        begin, end,
-                        [=](Index_type ijk) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), nn*nn*nn,
+                         [=](Index_type ijk) {
               const auto i  = 1 + ijk / (nn*nn);
               const auto jk = ijk % (nn*nn);
               const auto j  = 1 + jk / nn;
               const auto k  = 1 + jk % nn;
 #else
-          std::for_each( std::execution::par_unseq,
-                         begin, end,
-                         [=](Index_type i) {
-            std::for_each( std::execution::unseq,
-                           begin, end,
-                           [=](Index_type j) {
-              std::for_each( std::execution::unseq,
-                           begin, end,
-                           [=](Index_type k) {
+          std::for_each_n( std::execution::par_unseq,
+                           counting_iterator<Index_type>(1), N-2,
+                           [=](Index_type i) {
+            std::for_each_n( std::execution::unseq,
+                             counting_iterator<Index_type>(1), N-2,
+                             [=](Index_type j) {
+              std::for_each_n( std::execution::unseq,
+                               counting_iterator<Index_type>(1), N-2,
+                               [=](Index_type k) {
 #endif
                 POLYBENCH_HEAT_3D_BODY2;
 #ifndef USE_STDPAR_COLLAPSE
@@ -124,23 +119,23 @@ void POLYBENCH_HEAT_3D::runStdParVariant(VariantID vid, size_t tune_idx)
         for (Index_type t = 0; t < tsteps; ++t) {
 
 #ifdef USE_STDPAR_COLLAPSE
-        std::for_each( std::execution::par_unseq,
-                        begin, end,
-                        [=](Index_type ijk) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), nn*nn*nn,
+                         [=](Index_type ijk) {
               const auto i  = 1 + ijk / (nn*nn);
               const auto jk = ijk % (nn*nn);
               const auto j  = 1 + jk / nn;
               const auto k  = 1 + jk % nn;
 #else
-          std::for_each( std::execution::par_unseq,
-                         begin, end,
-                         [=](Index_type i) {
-            std::for_each( std::execution::unseq,
-                           begin, end,
-                           [=](Index_type j) {
-              std::for_each( std::execution::unseq,
-                           begin, end,
-                           [=](Index_type k) {
+          std::for_each_n( std::execution::par_unseq,
+                           counting_iterator<Index_type>(1), N-2,
+                           [=](Index_type i) {
+            std::for_each_n( std::execution::unseq,
+                             counting_iterator<Index_type>(1), N-2,
+                             [=](Index_type j) {
+              std::for_each_n( std::execution::unseq,
+                               counting_iterator<Index_type>(1), N-2,
+                               [=](Index_type k) {
 #endif
                 poly_heat3d_base_lam1(i, j, k);
 #ifndef USE_STDPAR_COLLAPSE
@@ -150,23 +145,23 @@ void POLYBENCH_HEAT_3D::runStdParVariant(VariantID vid, size_t tune_idx)
           });
 
 #ifdef USE_STDPAR_COLLAPSE
-        std::for_each( std::execution::par_unseq,
-                        begin, end,
-                        [=](Index_type ijk) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), nn*nn*nn,
+                         [=](Index_type ijk) {
               const auto i  = 1 + ijk / (nn*nn);
               const auto jk = ijk % (nn*nn);
               const auto j  = 1 + jk / nn;
               const auto k  = 1 + jk % nn;
 #else
-          std::for_each( std::execution::par_unseq,
-                         begin, end,
-                         [=](Index_type i) {
-            std::for_each( std::execution::unseq,
-                           begin, end,
-                           [=](Index_type j) {
-              std::for_each( std::execution::unseq,
-                           begin, end,
-                           [=](Index_type k) {
+          std::for_each_n( std::execution::par_unseq,
+                           counting_iterator<Index_type>(1), N-2,
+                           [=](Index_type i) {
+            std::for_each_n( std::execution::unseq,
+                             counting_iterator<Index_type>(1), N-2,
+                             [=](Index_type j) {
+              std::for_each_n( std::execution::unseq,
+                               counting_iterator<Index_type>(1), N-2,
+                               [=](Index_type k) {
 #endif
                 poly_heat3d_base_lam2(i, j, k);
 #ifndef USE_STDPAR_COLLAPSE
