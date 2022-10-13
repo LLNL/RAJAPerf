eb3ecee39481321d88291fc999ea5b170f3ddf05
Jeff Hammond <jeff.science@gmail.com>
2022-10-13 12:47:23 +0300

    remove lambda version - will not bother with this

--- src/basic/REDUCE_STRUCT-StdPar.cpp
+++ src/basic/REDUCE_STRUCT-StdPar.cpp
@@ -89,46 +89,6 @@ void REDUCE_STRUCT::runStdParVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(t
       break;
     }
 
-    case Lambda_StdPar : {
-
-      auto reduce_struct_x_base_lam = [=](Index_type i) -> Real_type {
-                                   return points.x[i];
-                                 };
-
-      auto reduce_struct_y_base_lam = [=](Index_type i) -> Real_type {
-                                   return points.y[i];
-                                 };
-
-      startTimer();
-      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
-
-        Real_type xsum = m_init_sum; Real_type ysum = m_init_sum;
-        Real_type xmin = m_init_min; Real_type ymin = m_init_min;
-        Real_type xmax = m_init_max; Real_type ymax = m_init_max; 
-
-#warning needs parallel
-        for (Index_type i = ibegin; i < iend; ++i ) {
-          xsum += reduce_struct_x_base_lam(i);
-          xmin = std::min(xmin, reduce_struct_x_base_lam(i));
-          xmax = std::max(xmax, reduce_struct_x_base_lam(i));
-          ysum += reduce_struct_y_base_lam(i);
-          ymin = std::min(ymin, reduce_struct_y_base_lam(i));
-          ymax = std::max(ymax, reduce_struct_y_base_lam(i));
-        }
-
-        points.SetCenter(xsum/(points.N), ysum/(points.N));
-        points.SetXMin(xmin); 
-        points.SetXMax(xmax);
-        points.SetYMin(ymin); 
-        points.SetYMax(ymax);
-        m_points=points;
-
-      }
-      stopTimer();
-
-      break;
-    }
-
     default : {
       getCout() << "\n  REDUCE_STRUCT : Unknown variant id = " << vid << std::endl;
     }
