6853072cf276d45697112a80f5cb1b52aa053831
Jeff Hammond <jeff.science@gmail.com>
2022-10-13 10:53:45 +0300

    for_each_n

--- src/polybench/POLYBENCH_3MM-StdPar.cpp
+++ src/polybench/POLYBENCH_3MM-StdPar.cpp
@@ -14,8 +14,6 @@
 
 #include <iostream>
 
-//#define USE_STDPAR_COLLAPSE 1
-
 namespace rajaperf 
 {
 namespace polybench
@@ -29,26 +27,6 @@ void POLYBENCH_3MM::runStdParVariant(VariantID vid, size_t tune_idx)
 
   POLYBENCH_3MM_DATA_SETUP;
 
-#ifdef USE_STDPAR_COLLAPSE
-  counting_iterator<Index_type> beginIJ(0);
-  counting_iterator<Index_type> endIJ(ni*nj);
-  counting_iterator<Index_type> beginIL(0);
-  counting_iterator<Index_type> endIL(ni*nl);
-  counting_iterator<Index_type> beginJL(0);
-  counting_iterator<Index_type> endJL(nj*nl);
-#else
-  counting_iterator<Index_type> beginI(0);
-  counting_iterator<Index_type> endI(ni);
-  counting_iterator<Index_type> beginL(0);
-  counting_iterator<Index_type> endL(nl);
-#endif
-  counting_iterator<Index_type> beginJ(0);
-  counting_iterator<Index_type> endJ(nj);
-  counting_iterator<Index_type> beginK(0);
-  counting_iterator<Index_type> endK(nk);
-  counting_iterator<Index_type> beginM(0);
-  counting_iterator<Index_type> endM(nm);
-
   switch ( vid ) {
 
     case Base_StdPar : {
@@ -57,17 +35,21 @@ void POLYBENCH_3MM::runStdParVariant(VariantID vid, size_t tune_idx)
       for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
 
 #ifdef USE_STDPAR_COLLAPSE
-        std::for_each( std::execution::par_unseq,
-                       beginIJ, endIJ, [=](Index_type ij) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), ni*nj,
+                         [=](Index_type ij) {
             const auto i  = ij / nj;
             const auto j  = ij % nj;
 #else
-        std::for_each( std::execution::par_unseq,
-                       beginI, endI, [=](Index_type i) {
-          std::for_each(beginJ, endJ, [=](Index_type j) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), ni,
+                         [=](Index_type i) {
+          std::for_each_n( counting_iterator<Index_type>(0), nj,
+                           [=](Index_type j) {
 #endif
             POLYBENCH_3MM_BODY1;
-            std::for_each(beginK, endK, [=,&dot](Index_type k) {
+            std::for_each_n( counting_iterator<Index_type>(0), nk,
+                             [=,&dot](Index_type k) {
               POLYBENCH_3MM_BODY2;
             });
             POLYBENCH_3MM_BODY3;
@@ -77,17 +59,21 @@ void POLYBENCH_3MM::runStdParVariant(VariantID vid, size_t tune_idx)
         });
 
 #ifdef USE_STDPAR_COLLAPSE
-        std::for_each( std::execution::par_unseq,
-                       beginJL, endJL, [=](Index_type jl) {
+        std::for_each_n( std::execution::par_unseq,
+                           counting_iterator<Index_type>(0), nj*nl,
+                         [=](Index_type jl) {
             const auto j  = jl / nl;
             const auto l  = jl % nl;
 #else
-        std::for_each( std::execution::par_unseq,
-                      beginJ, endJ, [=](Index_type j) {
-          std::for_each(beginL, endL, [=](Index_type l) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), nj,
+                         [=](Index_type j) {
+          std::for_each_n( counting_iterator<Index_type>(0), nl,
+                           [=](Index_type l) {
 #endif
             POLYBENCH_3MM_BODY4;
-            std::for_each(beginM, endM, [=,&dot](Index_type m) {
+            std::for_each_n( counting_iterator<Index_type>(0), nm,
+                             [=,&dot](Index_type m) {
               POLYBENCH_3MM_BODY5;
             });
             POLYBENCH_3MM_BODY6;
@@ -97,17 +83,21 @@ void POLYBENCH_3MM::runStdParVariant(VariantID vid, size_t tune_idx)
         });
 
 #ifdef USE_STDPAR_COLLAPSE
-        std::for_each( std::execution::par_unseq,
-                       beginIL, endIL, [=](Index_type il) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), ni*nl,
+                         [=](Index_type il) {
             const auto i  = il / nl;
             const auto l  = il % nl;
 #else
-        std::for_each( std::execution::par_unseq,
-                       beginI, endI, [=](Index_type i) {
-          std::for_each(beginL, endL, [=](Index_type l) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), ni,
+                         [=](Index_type i) {
+          std::for_each_n( counting_iterator<Index_type>(0), nl,
+                           [=](Index_type l) {
 #endif
             POLYBENCH_3MM_BODY7;
-            std::for_each(beginJ, endJ, [=,&dot](Index_type j) {
+            std::for_each_n( counting_iterator<Index_type>(0), nj,
+                             [=,&dot](Index_type j) {
               POLYBENCH_3MM_BODY8;
             });
             POLYBENCH_3MM_BODY9;
@@ -124,28 +114,28 @@ void POLYBENCH_3MM::runStdParVariant(VariantID vid, size_t tune_idx)
 
     case Lambda_StdPar : {
 
-      auto poly_3mm_base_lam2 = [=] (Index_type i, Index_type j, Index_type k,
-                                     Real_type &dot) {
+      auto poly_3mm_base_lam2 =
+              [=] (Index_type i, Index_type j, Index_type k, Real_type &dot) {
                                   POLYBENCH_3MM_BODY2;
                                 };
-      auto poly_3mm_base_lam3 = [=] (Index_type i, Index_type j,
-                                     Real_type &dot) {
+      auto poly_3mm_base_lam3 =
+              [=] (Index_type i, Index_type j, Real_type &dot) {
                                   POLYBENCH_3MM_BODY3;
                                 };
-      auto poly_3mm_base_lam5 = [=] (Index_type j, Index_type l, Index_type m,
-                                     Real_type &dot) {
+      auto poly_3mm_base_lam5 =
+              [=] (Index_type j, Index_type l, Index_type m, Real_type &dot) {
                                    POLYBENCH_3MM_BODY5;
                                 };
-      auto poly_3mm_base_lam6 = [=] (Index_type j, Index_type l,
-                                     Real_type &dot) {
+      auto poly_3mm_base_lam6 =
+              [=] (Index_type j, Index_type l, Real_type &dot) {
                                   POLYBENCH_3MM_BODY6;
                                 };
-      auto poly_3mm_base_lam8 = [=] (Index_type i, Index_type l, Index_type j,
-                                     Real_type &dot) {
+      auto poly_3mm_base_lam8 =
+              [=] (Index_type i, Index_type l, Index_type j, Real_type &dot) {
                                   POLYBENCH_3MM_BODY8;
                                 };
-      auto poly_3mm_base_lam9 = [=] (Index_type i, Index_type l,
-                                     Real_type &dot) {
+      auto poly_3mm_base_lam9 =
+              [=] (Index_type i, Index_type l, Real_type &dot) {
                                   POLYBENCH_3MM_BODY9;
                                 };
 
@@ -153,17 +143,21 @@ void POLYBENCH_3MM::runStdParVariant(VariantID vid, size_t tune_idx)
       for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
 
 #ifdef USE_STDPAR_COLLAPSE
-        std::for_each( std::execution::par_unseq,
-                       beginIJ, endIJ, [=](Index_type ij) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), ni*nj,
+                         [=](Index_type ij) {
             const auto i  = ij / nj;
             const auto j  = ij % nj;
 #else
-        std::for_each( std::execution::par_unseq,
-                       beginI, endI, [=](Index_type i) {
-          std::for_each(beginJ, endJ, [=](Index_type j) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), ni,
+                         [=](Index_type i) {
+          std::for_each_n( counting_iterator<Index_type>(0), nj,
+                           [=](Index_type j) {
 #endif
             POLYBENCH_3MM_BODY1;
-            std::for_each(beginK, endK, [=,&dot](Index_type k) {
+            std::for_each_n( counting_iterator<Index_type>(0), nk,
+                             [=,&dot](Index_type k) {
               poly_3mm_base_lam2(i, j, k, dot);
             });
             poly_3mm_base_lam3(i, j, dot);
@@ -173,17 +167,21 @@ void POLYBENCH_3MM::runStdParVariant(VariantID vid, size_t tune_idx)
         });
 
 #ifdef USE_STDPAR_COLLAPSE
-        std::for_each( std::execution::par_unseq,
-                       beginJL, endJL, [=](Index_type jl) {
+        std::for_each_n( std::execution::par_unseq,
+                           counting_iterator<Index_type>(0), nj*nl,
+                           [=](Index_type jl) {
             const auto j  = jl / nl;
             const auto l  = jl % nl;
 #else
-        std::for_each( std::execution::par_unseq,
-                       beginJ, endJ, [=](Index_type j) {
-          std::for_each(beginL, endL, [=](Index_type l) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), nj,
+                         [=](Index_type j) {
+          std::for_each_n( counting_iterator<Index_type>(0), nl,
+                           [=](Index_type l) {
 #endif
             POLYBENCH_3MM_BODY4;
-            std::for_each(beginM, endM, [=,&dot](Index_type m) {
+            std::for_each_n( counting_iterator<Index_type>(0), nm,
+                             [=,&dot](Index_type m) {
               poly_3mm_base_lam5(j, l, m, dot);
             });
             poly_3mm_base_lam6(j, l, dot);
@@ -193,17 +191,21 @@ void POLYBENCH_3MM::runStdParVariant(VariantID vid, size_t tune_idx)
         });
 
 #ifdef USE_STDPAR_COLLAPSE
-        std::for_each( std::execution::par_unseq,
-                       beginIL, endIL, [=](Index_type il) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), ni*nl,
+                         [=](Index_type il) {
             const auto i  = il / nl;
             const auto l  = il % nl;
 #else
-        std::for_each( std::execution::par_unseq,
-                       beginI, endI, [=](Index_type i) {
-          std::for_each(beginL, endL, [=](Index_type l) {
+        std::for_each_n( std::execution::par_unseq,
+                         counting_iterator<Index_type>(0), ni,
+                         [=](Index_type i) {
+          std::for_each_n( counting_iterator<Index_type>(0), nl,
+                           [=](Index_type l) {
 #endif
             POLYBENCH_3MM_BODY7;
-            std::for_each(beginJ, endJ, [=,&dot](Index_type j) {
+            std::for_each_n( counting_iterator<Index_type>(0), nj,
+                             [=,&dot](Index_type j) {
               poly_3mm_base_lam8(i, l, j, dot);
             });
             poly_3mm_base_lam9(i, l, dot);
