3392e31f7cd1c370edd917f30a4bc3cfd3fe58c0
Thorsten Blass <Thorsten.Blass@amd.com>
2022-11-10 18:10:30 +0000

    Changes made by clang-format undone.

--- src/algorithm/MEMCPY-OMPTarget.cpp
+++ src/algorithm/MEMCPY-OMPTarget.cpp
@@ -29,63 +29,63 @@ namespace algorithm
 #define MEMCPY_DATA_SETUP_OMP_TARGET \
   int hid = omp_get_initial_device(); \
   int did = omp_get_default_device(); \
-                                      \
+\
   allocAndInitOpenMPDeviceData(x, m_x, iend, did, hid); \
   allocAndInitOpenMPDeviceData(y, m_y, iend, did, hid);
 
-#define MEMCPY_DATA_TEARDOWN_OMP_TARGET                                        \
-  deallocOpenMPDeviceData(x, did);                                             \
+#define MEMCPY_DATA_TEARDOWN_OMP_TARGET \
+  deallocOpenMPDeviceData(x, did); \
   deallocOpenMPDeviceData(y, did);
 
 
 void MEMCPY::runOpenMPTargetVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune_idx))
 {
-    const Index_type run_reps = getRunReps();
-    const Index_type ibegin = 0;
-    const Index_type iend = getActualProblemSize();
+  const Index_type run_reps = getRunReps();
+  const Index_type ibegin = 0;
+  const Index_type iend = getActualProblemSize();
 
-    MEMCPY_DATA_SETUP;
+  MEMCPY_DATA_SETUP;
 
   if ( vid == Base_OpenMPTarget ) {
 
-      MEMCPY_DATA_SETUP_OMP_TARGET
+    MEMCPY_DATA_SETUP_OMP_TARGET
 
-      startTimer();
-      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
+    startTimer();
+    for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
 
       #pragma omp target is_device_ptr(x, y) device( did )
       #pragma omp teams distribute parallel for \
               thread_limit(threads_per_team) schedule(static, 1)
       for (Index_type i = ibegin; i < iend; ++i ) {
-          MEMCPY_BODY;
-        }
-
+        MEMCPY_BODY;
       }
-      stopTimer();
 
-      MEMCPY_DATA_TEARDOWN_OMP_TARGET
+    }
+    stopTimer();
+
+    MEMCPY_DATA_TEARDOWN_OMP_TARGET
 
   } else if ( vid == RAJA_OpenMPTarget ) {
 
-      MEMCPY_DATA_SETUP_OMP_TARGET
+    MEMCPY_DATA_SETUP_OMP_TARGET
 
-      startTimer();
-      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
+    startTimer();
+    for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
 
-        RAJA::forall<RAJA::omp_target_parallel_for_exec<threads_per_team>>(
-            RAJA::RangeSegment(ibegin, iend),
+      RAJA::forall<RAJA::omp_target_parallel_for_exec<threads_per_team>>(
+        RAJA::RangeSegment(ibegin, iend),
         [=](Index_type i) {
           MEMCPY_BODY;
       });
 
-      }
-      stopTimer();
-
-      MEMCPY_DATA_TEARDOWN_OMP_TARGET
-
-    } else {
-    getCout() << "\n  MEMCPY : Unknown OMP Target variant id = " << vid << std::endl;
     }
+    stopTimer();
+
+    MEMCPY_DATA_TEARDOWN_OMP_TARGET
+
+  } else {
+    getCout() << "\n  MEMCPY : Unknown OMP Target variant id = " << vid << std::endl;
+  }
 
 }
 
--- src/basic/REDUCE_STRUCT-OMPTarget.cpp
+++ src/basic/REDUCE_STRUCT-OMPTarget.cpp
@@ -30,34 +30,35 @@ namespace basic
   int hid = omp_get_initial_device(); \
   int did = omp_get_default_device(); \
 \
-  allocAndInitOpenMPDeviceData(points.x, m_x, points.N, did, hid);             \
-  allocAndInitOpenMPDeviceData(points.y, m_y, points.N, did, hid);
+  allocAndInitOpenMPDeviceData(points.x, m_x, points.N, did, hid); \
+  allocAndInitOpenMPDeviceData(points.y, m_y, points.N, did, hid); 
 
-#define REDUCE_STRUCT_DATA_TEARDOWN_OMP_TARGET                                 \
-  deallocOpenMPDeviceData(points.x, did);                                           \
-  deallocOpenMPDeviceData(points.y, did);
+#define REDUCE_STRUCT_DATA_TEARDOWN_OMP_TARGET \
+  deallocOpenMPDeviceData(points.x, did); \
+  deallocOpenMPDeviceData(points.y, did); \
 
-  void
-  REDUCE_STRUCT::runOpenMPTargetVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune_idx)) {
-    const Index_type run_reps = getRunReps();
-    const Index_type ibegin = 0;
-    const Index_type iend = getActualProblemSize();
 
-    REDUCE_STRUCT_DATA_SETUP;
+void REDUCE_STRUCT::runOpenMPTargetVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune_idx))
+{
+  const Index_type run_reps = getRunReps();
+  const Index_type ibegin = 0;
+  const Index_type iend = getActualProblemSize();
+
+  REDUCE_STRUCT_DATA_SETUP;
 
   if ( vid == Base_OpenMPTarget ) {
 
-      REDUCE_STRUCT_DATA_SETUP_OMP_TARGET;
+    REDUCE_STRUCT_DATA_SETUP_OMP_TARGET;
 
-      startTimer();
-      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
+    startTimer();
+    for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
 
       Real_type xsum = m_init_sum; Real_type ysum = m_init_sum;
       Real_type xmin = m_init_min; Real_type ymin = m_init_min;
       Real_type xmax = m_init_max; Real_type ymax = m_init_max;
 
-      #pragma omp target is_device_ptr(vec) device( did ) map(tofrom:xsum, xmin, xmax, ysum, ymin, ymax)
-#pragma omp teams distribute parallel for thread_limit(threads_per_team) schedule(static,1) \
+      #pragma omp target device( did ) map(tofrom:xsum, xmin, xmax, ysum, ymin, ymax)
+      #pragma omp teams distribute parallel for thread_limit(threads_per_team) schedule(static,1) \
                                reduction(+:xsum) \
                                reduction(min:xmin) \
                                reduction(max:xmax), \
@@ -71,53 +72,53 @@ namespace basic
       points.SetCenter(xsum/points.N, ysum/points.N);
       points.SetXMin(xmin);
       points.SetXMax(xmax);
-      points.SetYMin(ymin);
+      points.SetYMin(ymin); 
       points.SetYMax(ymax);
       m_points=points;
 
-      }
-      stopTimer();
+    }
+    stopTimer();
 
-      REDUCE_STRUCT_DATA_TEARDOWN_OMP_TARGET;
+    REDUCE_STRUCT_DATA_TEARDOWN_OMP_TARGET;
 
   } else if ( vid == RAJA_OpenMPTarget ) {
 
-      REDUCE_STRUCT_DATA_SETUP_OMP_TARGET;
+    REDUCE_STRUCT_DATA_SETUP_OMP_TARGET;
 
-      startTimer();
-      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
+    startTimer();
+    for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
 
-        RAJA::ReduceSum<RAJA::omp_target_reduce, Real_type> xsum(m_init_sum);
-        RAJA::ReduceSum<RAJA::omp_target_reduce, Real_type> ysum(m_init_sum);
-        RAJA::ReduceMin<RAJA::omp_target_reduce, Real_type> xmin(m_init_min);
-        RAJA::ReduceMin<RAJA::omp_target_reduce, Real_type> ymin(m_init_min);
-        RAJA::ReduceMax<RAJA::omp_target_reduce, Real_type> xmax(m_init_max);
-        RAJA::ReduceMax<RAJA::omp_target_reduce, Real_type> ymax(m_init_max);
+      RAJA::ReduceSum<RAJA::omp_target_reduce, Real_type> xsum(m_init_sum);
+      RAJA::ReduceSum<RAJA::omp_target_reduce, Real_type> ysum(m_init_sum);
+      RAJA::ReduceMin<RAJA::omp_target_reduce, Real_type> xmin(m_init_min);
+      RAJA::ReduceMin<RAJA::omp_target_reduce, Real_type> ymin(m_init_min);
+      RAJA::ReduceMax<RAJA::omp_target_reduce, Real_type> xmax(m_init_max);
+      RAJA::ReduceMax<RAJA::omp_target_reduce, Real_type> ymax(m_init_max);
 
-        RAJA::forall<RAJA::omp_target_parallel_for_exec<threads_per_team>>(
-            RAJA::RangeSegment(ibegin, iend),
+      RAJA::forall<RAJA::omp_target_parallel_for_exec<threads_per_team>>(
+        RAJA::RangeSegment(ibegin, iend),
         [=](Index_type i) {
         REDUCE_STRUCT_BODY_RAJA;
       });
 
-        points.SetCenter(xsum.get()/(points.N),
-                         ysum.get()/(points.N));
-        points.SetXMin(xmin.get());
-        points.SetXMax(xmax.get());
-        points.SetYMin(ymin.get());
-        points.SetYMax(ymax.get());
-        m_points=points;
+      points.SetCenter(xsum.get()/(points.N),
+                       ysum.get()/(points.N));
+      points.SetXMin(xmin.get());
+      points.SetXMax(xmax.get());
+      points.SetYMin(ymin.get());
+      points.SetYMax(ymax.get());
+      m_points=points;
 
-      }
-      stopTimer();
-
-      REDUCE_STRUCT_DATA_TEARDOWN_OMP_TARGET;
-
-    } else {
-     getCout() << "\n  REDUCE_STRUCT : Unknown OMP Target variant id = " << vid << std::endl;
     }
+    stopTimer();
 
+    REDUCE_STRUCT_DATA_TEARDOWN_OMP_TARGET;
+
+  } else {
+     getCout() << "\n  REDUCE_STRUCT : Unknown OMP Target variant id = " << vid << std::endl;
   }
+
+}
 
 } // end namespace basic
 } // end namespace rajaperf
