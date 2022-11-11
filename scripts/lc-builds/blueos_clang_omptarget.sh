c850a2bc447c98b028b34691546c8c011616fb5b
Jason Burmark <burmark1@llnl.gov>
2022-11-11 10:26:01 -0800

    Fix flag in build script

--- scripts/lc-builds/blueos_clang_omptarget.sh
+++ scripts/lc-builds/blueos_clang_omptarget.sh
@@ -40,7 +40,7 @@ cmake \
   -C ${RAJA_HOSTCONFIG} \
   -DENABLE_OPENMP=On \
   -DENABLE_CUDA=Off \
-  -DENABLE_TARGET_OPENMP=On \
+  -DRAJA_ENABLE_TARGET_OPENMP=On \
   -DOpenMP_CXX_FLAGS="-fopenmp;-fopenmp-targets=nvptx64-nvidia-cuda" \
   -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
   "$@" \
