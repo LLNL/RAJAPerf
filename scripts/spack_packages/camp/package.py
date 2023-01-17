52ad2186b3fd1cb63821aaacd674f93b88dabc3e
Adrien M. BERNEDE <a.bernede@woptim.dev>
2023-01-17 12:18:13 +0100

    Update Camp Spack package

--- scripts/spack_packages/camp/package.py
+++ scripts/spack_packages/camp/package.py
@@ -3,10 +3,22 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import os
 import glob
+import re
 
 from spack.package import *
 
+
+def spec_uses_toolchain(spec):
+    gcc_toolchain_regex = re.compile(".*gcc-toolchain.*")
+    using_toolchain = list(filter(gcc_toolchain_regex.match, spec.compiler_flags["cxxflags"]))
+    return using_toolchain
+
+def spec_uses_gccname(spec):
+    gcc_name_regex = re.compile(".*gcc-name.*")
+    using_gcc_name = list(filter(gcc_name_regex.match, spec.compiler_flags["cxxflags"]))
+    return using_gcc_name
 
 def hip_repair_options(options, spec):
     # there is only one dir like this, but the version component is unknown
@@ -14,7 +26,6 @@ def hip_repair_options(options, spec):
         "-DHIP_CLANG_INCLUDE_PATH="
         + glob.glob("{}/lib/clang/*/include".format(spec["llvm-amdgpu"].prefix))[0]
     )
-
 
 def hip_repair_cache(options, spec):
     # there is only one dir like this, but the version component is unknown
@@ -24,6 +35,81 @@ def hip_repair_cache(options, spec):
             glob.glob("{}/lib/clang/*/include".format(spec["llvm-amdgpu"].prefix))[0],
         )
     )
+
+def hip_for_radiuss_projects(options, spec, spec_compiler):
+    # Here is what is typically needed for radiuss projects when building with rocm
+    hip_root = spec["hip"].prefix
+    rocm_root = hip_root + "/.."
+    options.append(cmake_cache_path("HIP_ROOT_DIR", hip_root))
+    options.append(cmake_cache_path("ROCM_ROOT_DIR", rocm_root))
+
+    hip_repair_cache(options, spec)
+
+    archs = spec.variants["amdgpu_target"].value
+    if archs != "none":
+        arch_str = ",".join(archs)
+        options.append(
+            cmake_cache_string("HIP_HIPCC_FLAGS", "--amdgpu-target={0}".format(arch_str))
+        )
+        options.append(
+            cmake_cache_string("CMAKE_HIP_ARCHITECTURES", arch_str)
+        )
+
+    # adrienbernede-22-11:
+    #   Specific to Umpire, attempt port to RAJA and CHAI
+    hip_link_flags = ""
+    if "%gcc" in spec:
+        gcc_bin = os.path.dirname(spec_compiler.cxx)
+        gcc_prefix = join_path(gcc_bin, "..")
+        options.append(cmake_cache_string("HIP_CLANG_FLAGS", "--gcc-toolchain={0}".format(gcc_prefix)))
+        options.append(cmake_cache_string("CMAKE_EXE_LINKER_FLAGS", hip_link_flags + " -Wl,-rpath {}/lib64".format(gcc_prefix)))
+    else:
+        options.append(cmake_cache_string("CMAKE_EXE_LINKER_FLAGS", "-Wl,-rpath={0}/llvm/lib/".format(rocm_root)))
+
+def cuda_for_radiuss_projects(options, spec):
+    # Here is what is typically needed for radiuss projects when building with cuda
+
+    cuda_flags = []
+    if not spec.satisfies("cuda_arch=none"):
+        cuda_arch = spec.variants["cuda_arch"].value
+        cuda_flags.append("-arch sm_{0}".format(cuda_arch[0]))
+        options.append(
+            cmake_cache_string("CUDA_ARCH", "sm_{0}".format(cuda_arch[0])))
+        options.append(
+            cmake_cache_string("CMAKE_CUDA_ARCHITECTURES", "{0}".format(cuda_arch[0])))
+    if spec_uses_toolchain(spec):
+        cuda_flags.append("-Xcompiler {}".format(spec_uses_toolchain(spec)[0]))
+    if (spec.satisfies("%gcc@8.1: target=ppc64le")):
+        cuda_flags.append("-Xcompiler -mno-float128")
+    options.append(cmake_cache_string("CMAKE_CUDA_FLAGS", " ".join(cuda_flags)))
+
+def blt_link_helpers(options, spec, spec_compiler):
+
+    ### From local package:
+    fortran_compilers = ["gfortran", "xlf"]
+    if any(compiler in spec_compiler.fc for compiler in fortran_compilers) and ("clang" in spec_compiler.cxx):
+        # Pass fortran compiler lib as rpath to find missing libstdc++
+        libdir = os.path.join(os.path.dirname(
+                       os.path.dirname(spec_compiler.fc)), "lib")
+        flags = ""
+        for _libpath in [libdir, libdir + "64"]:
+            if os.path.exists(_libpath):
+                flags += " -Wl,-rpath,{0}".format(_libpath)
+        description = ("Adds a missing libstdc++ rpath")
+        if flags:
+            options.append(cmake_cache_string("BLT_EXE_LINKER_FLAGS", flags, description))
+
+        # Ignore conflicting default gcc toolchain
+        options.append(cmake_cache_string("BLT_CMAKE_IMPLICIT_LINK_DIRECTORIES_EXCLUDE",
+        "/usr/tce/packages/gcc/gcc-4.9.3/lib64;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64/gcc/powerpc64le-unknown-linux-gnu/4.9.3;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64;/usr/tce/packages/gcc/gcc-4.9.3/lib64/gcc/x86_64-unknown-linux-gnu/4.9.3"))
+
+    compilers_using_toolchain = ["pgi", "xl", "icpc"]
+    if any(compiler in spec_compiler.cxx for compiler in compilers_using_toolchain):
+        if spec_uses_toolchain(spec) or spec_uses_gccname(spec):
+
+            # Ignore conflicting default gcc toolchain
+            options.append(cmake_cache_string("BLT_CMAKE_IMPLICIT_LINK_DIRECTORIES_EXCLUDE",
+            "/usr/tce/packages/gcc/gcc-4.9.3/lib64;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64/gcc/powerpc64le-unknown-linux-gnu/4.9.3;/usr/tce/packages/gcc/gcc-4.9.3/gnu/lib64;/usr/tce/packages/gcc/gcc-4.9.3/lib64/gcc/x86_64-unknown-linux-gnu/4.9.3"))
 
 
 class Camp(CMakePackage, CudaPackage, ROCmPackage):
@@ -38,7 +124,8 @@ class Camp(CMakePackage, CudaPackage, ROCmPackage):
 
     maintainers = ["trws"]
 
-    version("main", branch="main", submodules="True")
+    version("main", branch="main", submodules="False")
+    version("2022.10.1", sha256="2d12f1a46f5a6d01880fc075cfbd332e2cf296816a7c1aa12d4ee5644d386f02")
     version("2022.10.0", sha256="3561c3ef00bbcb61fe3183c53d49b110e54910f47e7fc689ad9ccce57e55d6b8")
     version("2022.03.2", sha256="bc4aaeacfe8f2912e28f7a36fc731ab9e481bee15f2c6daf0cb208eed3f201eb")
     version("2022.03.0", sha256="e9090d5ee191ea3a8e36b47a8fe78f3ac95d51804f1d986d931e85b8f8dad721")
