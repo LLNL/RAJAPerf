ed75ae81643ae0fef5021628f7e0755796deddee
Jason Burmark <burmark1@llnl.gov>
2023-03-24 15:02:56 -0700

    Only print Data Spaces for used backends

--- src/common/Executor.cpp
+++ src/common/Executor.cpp
@@ -718,11 +718,21 @@ void Executor::reportRunSummary(ostream& str) const
     str << "\nData Spaces"
         << "\n--------";
     str << "\nSeq - " << getDataSpaceName(run_params.getSeqDataSpace());
-    str << "\nOpenMP - " << getDataSpaceName(run_params.getOmpDataSpace());
-    str << "\nOpenMP Target - " << getDataSpaceName(run_params.getOmpTargetDataSpace());
-    str << "\nCuda - " << getDataSpaceName(run_params.getCudaDataSpace());
-    str << "\nHip - " << getDataSpaceName(run_params.getHipDataSpace());
-    str << "\nKokkos - " << getDataSpaceName(run_params.getKokkosDataSpace());
+    if (isVariantAvailable(VariantID::Base_OpenMP)) {
+      str << "\nOpenMP - " << getDataSpaceName(run_params.getOmpDataSpace());
+    }
+    if (isVariantAvailable(VariantID::Base_OpenMPTarget)) {
+      str << "\nOpenMP Target - " << getDataSpaceName(run_params.getOmpTargetDataSpace());
+    }
+    if (isVariantAvailable(VariantID::Base_CUDA)) {
+      str << "\nCuda - " << getDataSpaceName(run_params.getCudaDataSpace());
+    }
+    if (isVariantAvailable(VariantID::Base_HIP)) {
+      str << "\nHip - " << getDataSpaceName(run_params.getHipDataSpace());
+    }
+    if (isVariantAvailable(VariantID::Kokkos_Lambda)) {
+      str << "\nKokkos - " << getDataSpaceName(run_params.getKokkosDataSpace());
+    }
     str << endl;
 
     str << "\nVariants and Tunings"
