489bc32999ac66b95b43fcaa062e462561776862
Jason Burmark <burmark1@llnl.gov>
2023-03-24 13:26:12 -0700

    Add include for exceptions

--- src/common/DataUtils.cpp
+++ src/common/DataUtils.cpp
@@ -16,6 +16,7 @@
 
 #include <cstdlib>
 #include <cstring>
+#include <stdexcept>
 #include <unistd.h>
 
 namespace rajaperf
--- src/common/KernelBase.cpp
+++ src/common/KernelBase.cpp
@@ -13,6 +13,7 @@
 
 #include <cmath>
 #include <limits>
+#include <stdexcept>
 
 namespace rajaperf {
 
