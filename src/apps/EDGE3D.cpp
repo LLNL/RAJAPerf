8c0f9fc11e7aa408d16ef6a9fd4c61e6827a151c
Rich Hornung <hornung1@llnl.gov>
2023-04-18 11:51:08 -0700

    Need to pass variant id to calcChecksum

--- src/apps/EDGE3D.cpp
+++ src/apps/EDGE3D.cpp
@@ -108,7 +108,7 @@ void EDGE3D::setUp(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune_idx))
 
 void EDGE3D::updateChecksum(VariantID vid, size_t tune_idx)
 {
-  checksum[vid][tune_idx] += calcChecksum(m_sum, m_array_length, checksum_scale_factor );
+  checksum[vid][tune_idx] += calcChecksum(m_sum, m_array_length, checksum_scale_factor, vid  );
 }
 
 void EDGE3D::tearDown(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune_idx))
