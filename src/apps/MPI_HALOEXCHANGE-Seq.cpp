749f24fafef736a63aaa827ce3a1349a7ecae3b3
Jason Burmark <burmark1@llnl.gov>
2023-06-06 08:47:15 -0700

    fix spacing

--- src/apps/MPI_HALOEXCHANGE-Seq.cpp
+++ src/apps/MPI_HALOEXCHANGE-Seq.cpp
@@ -106,7 +106,7 @@ void MPI_HALOEXCHANGE::runSeqVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(t
         for (Index_type l = 0; l < num_neighbors; ++l) {
           Real_ptr buffer = pack_buffers[l];
           Int_ptr list = pack_index_lists[l];
-          Index_type  len  = pack_index_list_lengths[l];
+          Index_type len = pack_index_list_lengths[l];
           for (Index_type v = 0; v < num_vars; ++v) {
             Real_ptr var = vars[v];
             auto haloexchange_pack_base_lam = [=](Index_type i) {
