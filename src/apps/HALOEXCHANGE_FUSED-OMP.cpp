ac4375e2ca018d635d9d8b14e4e064977b68722c
Rich Hornung <hornung1@llnl.gov>
2023-07-13 10:55:03 -0700

    Change guard for omp task option

--- src/apps/HALOEXCHANGE_FUSED-OMP.cpp
+++ src/apps/HALOEXCHANGE_FUSED-OMP.cpp
@@ -49,8 +49,8 @@ void HALOEXCHANGE_FUSED::runOpenMPVariant(VariantID vid, size_t RAJAPERF_UNUSED_
             buffer += len;
           }
         }
-#if 0
-//#if _OPENMP >= 200805
+
+#if defined(RAJA_ENABLE_OMP_TASK_INTERNAL) 
         #pragma omp parallel
         #pragma omp single nowait
         for (Index_type j = 0; j < pack_index; j++) {
@@ -92,8 +92,8 @@ void HALOEXCHANGE_FUSED::runOpenMPVariant(VariantID vid, size_t RAJAPERF_UNUSED_
             buffer += len;
           }
         }
-#if 0
-//#if _OPENMP >= 200805
+
+#if defined(RAJA_ENABLE_OMP_TASK_INTERNAL) 
         #pragma omp parallel
         #pragma omp single nowait
         for (Index_type j = 0; j < unpack_index; j++) {
