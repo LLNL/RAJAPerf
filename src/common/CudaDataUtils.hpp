aa9f1b703c60acf0cb9ed6c31a7d168503d58ee8
Jason Burmark <burmark1@llnl.gov>
2023-07-25 16:24:55 -0700

    remove extra lambda indexers

--- src/common/CudaDataUtils.hpp
+++ src/common/CudaDataUtils.hpp
@@ -80,38 +80,6 @@ __global__ void lambda_cuda(Lambda body)
   body();
 }
 
-/*!
- * \brief Getters for cuda kernel indices.
- */
-template < typename Index >
-__device__ inline Index_type lambda_cuda_get_index();
-
-template < >
-__device__ inline Index_type lambda_cuda_get_index<RAJA::cuda_thread_x_direct>() {
-  return threadIdx.x;
-}
-template < >
-__device__ inline Index_type lambda_cuda_get_index<RAJA::cuda_thread_y_direct>() {
-  return threadIdx.y;
-}
-template < >
-__device__ inline Index_type lambda_cuda_get_index<RAJA::cuda_thread_z_direct>() {
-  return threadIdx.z;
-}
-
-template < >
-__device__ inline Index_type lambda_cuda_get_index<RAJA::cuda_block_x_direct>() {
-  return blockIdx.x;
-}
-template < >
-__device__ inline Index_type lambda_cuda_get_index<RAJA::cuda_block_y_direct>() {
-  return blockIdx.y;
-}
-template < >
-__device__ inline Index_type lambda_cuda_get_index<RAJA::cuda_block_z_direct>() {
-  return blockIdx.z;
-}
-
 
 namespace detail
 {
