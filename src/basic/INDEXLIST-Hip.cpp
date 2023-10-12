3f0cddd73d4fe915ff5c6c79ddb3003fad203743
Jason Burmark <burmark1@llnl.gov>
2023-10-12 11:13:04 -0700

    Remove now unnecessary sync

--- src/basic/INDEXLIST-Hip.cpp
+++ src/basic/INDEXLIST-Hip.cpp
@@ -264,7 +264,6 @@ void INDEXLIST::runHipVariantImpl(VariantID vid)
     allocData(DataSpace::HipDevice, grid_counts, grid_size);
     unsigned* block_readys;
     allocData(DataSpace::HipDevice, block_readys, grid_size);
-    hipErrchk( hipStreamSynchronize( res.get_stream() ) );
 
     startTimer();
     for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
