8c06b6647438b73ca81f7c30eff417f9a2ea73bc
Jason Burmark <burmark1@llnl.gov>
2023-11-01 08:02:22 -0700

    Switch default block size of TRIAD_PARTED_FUSED

--- src/stream/TRIAD_PARTED_FUSED.hpp
+++ src/stream/TRIAD_PARTED_FUSED.hpp
@@ -110,7 +110,7 @@ public:
   void runHipVariantImpl(VariantID vid);
 
 private:
-  static const size_t default_gpu_block_size = 1024;
+  static const size_t default_gpu_block_size = 256;
   using gpu_block_sizes_type = gpu_block_size::make_list_type<default_gpu_block_size>;
 
   std::vector<Index_type> m_parts;
