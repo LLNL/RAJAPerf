957f19893c38307838ee1fd30915ff591b422b44
Rich Hornung <hornung1@llnl.gov>
2024-07-03 14:50:34 -0700

    Add new reduction tunings of REDUCE_STRUCT kernel

--- src/basic/REDUCE_STRUCT-Seq.cpp
+++ src/basic/REDUCE_STRUCT-Seq.cpp
@@ -19,8 +19,11 @@ namespace basic
 {
 
 
-void REDUCE_STRUCT::runSeqVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune_idx))
+void REDUCE_STRUCT::runSeqVariant(VariantID vid, size_t tune_idx)
 {
+#if !defined(RUN_RAJA_SEQ)
+  RAJA_UNUSED_VAR(tune_idx);
+#endif
   const Index_type run_reps = getRunReps();
   const Index_type ibegin = 0;
   const Index_type iend = getActualProblemSize();
@@ -47,7 +50,7 @@ void REDUCE_STRUCT::runSeqVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune
         points.SetXMax(xmax);
         points.SetYMin(ymin);
         points.SetYMax(ymax);
-        m_points=points;
+        m_points = points;
 
       }
       stopTimer();
@@ -87,7 +90,7 @@ void REDUCE_STRUCT::runSeqVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune
         points.SetXMax(xmax);
         points.SetYMin(ymin); 
         points.SetYMax(ymax);
-        m_points=points;
+        m_points = points;
 
       }
       stopTimer();
@@ -97,31 +100,75 @@ void REDUCE_STRUCT::runSeqVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune
 
     case RAJA_Seq : {
 
-      startTimer();
-      for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
+      if (tune_idx == 0) {
 
-        RAJA::ReduceSum<RAJA::seq_reduce, Real_type> xsum(m_init_sum);
-        RAJA::ReduceSum<RAJA::seq_reduce, Real_type> ysum(m_init_sum);
-        RAJA::ReduceMin<RAJA::seq_reduce, Real_type> xmin(m_init_min);
-        RAJA::ReduceMin<RAJA::seq_reduce, Real_type> ymin(m_init_min);
-        RAJA::ReduceMax<RAJA::seq_reduce, Real_type> xmax(m_init_max);
-        RAJA::ReduceMax<RAJA::seq_reduce, Real_type> ymax(m_init_max);
+        startTimer();
+        for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
+  
+          RAJA::ReduceSum<RAJA::seq_reduce, Real_type> xsum(m_init_sum);
+          RAJA::ReduceSum<RAJA::seq_reduce, Real_type> ysum(m_init_sum);
+          RAJA::ReduceMin<RAJA::seq_reduce, Real_type> xmin(m_init_min);
+          RAJA::ReduceMin<RAJA::seq_reduce, Real_type> ymin(m_init_min);
+          RAJA::ReduceMax<RAJA::seq_reduce, Real_type> xmax(m_init_max);
+          RAJA::ReduceMax<RAJA::seq_reduce, Real_type> ymax(m_init_max);
+  
+          RAJA::forall<RAJA::seq_exec>(
+            RAJA::RangeSegment(ibegin, iend), [=](Index_type i) {
+            REDUCE_STRUCT_BODY_RAJA;
+          });
+  
+          points.SetCenter(xsum.get()/(points.N),
+                           ysum.get()/(points.N));
+          points.SetXMin(xmin.get());
+          points.SetXMax(xmax.get());
+          points.SetYMin(ymin.get());
+          points.SetYMax(ymax.get());
+          m_points = points;
 
-        RAJA::forall<RAJA::seq_exec>(
-        RAJA::RangeSegment(ibegin, iend), [=](Index_type i) {
-        REDUCE_STRUCT_BODY_RAJA;
-        });
-
-      	points.SetCenter(xsum.get()/(points.N),
-                         ysum.get()/(points.N));
-        points.SetXMin(xmin.get());
-        points.SetXMax(xmax.get());
-        points.SetYMin(ymin.get());
-        points.SetYMax(ymax.get());
-        m_points=points;
+        }
+        stopTimer();
 
       }
-      stopTimer();
+
+      if (tune_idx == 1) {
+
+        startTimer();
+        for (RepIndex_type irep = 0; irep < run_reps; ++irep) {
+
+          Real_type txsum = m_init_sum; 
+          Real_type tysum = m_init_sum; 
+          Real_type txmin = m_init_min; 
+          Real_type tymin = m_init_min; 
+          Real_type txmax = m_init_max; 
+          Real_type tymax = m_init_max; 
+ 
+          RAJA::forall<RAJA::seq_exec>(
+            RAJA::RangeSegment(ibegin, iend),
+            RAJA::expt::Reduce<RAJA::operators::plus>(&txsum),
+            RAJA::expt::Reduce<RAJA::operators::plus>(&tysum),
+            RAJA::expt::Reduce<RAJA::operators::minimum>(&txmin),
+            RAJA::expt::Reduce<RAJA::operators::minimum>(&tymin),
+            RAJA::expt::Reduce<RAJA::operators::maximum>(&txmax), 
+            RAJA::expt::Reduce<RAJA::operators::maximum>(&tymax), 
+            [=](Index_type i, Real_type& xsum, Real_type& ysum,
+                              Real_type& xmin, Real_type& ymin,
+                              Real_type& xmax, Real_type& ymax) {
+              REDUCE_STRUCT_BODY;
+            }
+          );
+ 
+          points.SetCenter(static_cast<Real_type>(txsum)/(points.N),
+                           static_cast<Real_type>(tysum)/(points.N));
+          points.SetXMin(static_cast<Real_type>(txmin));
+          points.SetXMax(static_cast<Real_type>(txmax));
+          points.SetYMin(static_cast<Real_type>(tymin));
+          points.SetYMax(static_cast<Real_type>(tymax));
+          m_points = points;
+
+        }
+        stopTimer();
+
+      }
 
       break;
     }
@@ -132,7 +179,14 @@ void REDUCE_STRUCT::runSeqVariant(VariantID vid, size_t RAJAPERF_UNUSED_ARG(tune
     }
 
   }
+}
 
+void REDUCE_STRUCT::setSeqTuningDefinitions(VariantID vid)
+{
+  addVariantTuningName(vid, "default");
+  if (vid == RAJA_Seq) {
+    addVariantTuningName(vid, "new");
+  }
 }
 
 } // end namespace basic
